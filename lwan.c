11794f6a78f77f5e5f535cbd78118ba3cc5eda01
Leandro Pereira <leandro@hardinfo.org>
2012-02-13 20:37:37 -0200

    It is unlikely that a request's URL doesn't begin with an '/'

--- lwan.c
+++ lwan.c
@@ -201,7 +201,7 @@ _identify_http_path(lwan_request_t *request, char *buffer, size_t limit)
     request->url = buffer;
     request->url_len = space - buffer;
 
-    if (*request->url != '/')
+    if (UNLIKELY(*request->url != '/'))
         return NULL;
 
     return end_of_line + 1;
