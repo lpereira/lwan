0d07b0698f923ebed5b2eb26d326de0ce3d0e50c
Leandro Pereira <leandro@hardinfo.org>
2013-04-21 09:34:59 -0300

    Rewrite benchmark.py to use weighttp
    Uses the patched weighttp found on git://github.com/lpereira/weighttp.git,
    which support JSON output with the ``-j'' command-line switch.

--- benchmark.py
+++ benchmark.py
@@ -1,69 +1,75 @@
 #!/usr/bin/python
 
+import sys
+import json
 import commands
-import pickle
-import re
-import os
-import numpy
+import time
 
-RE_RPS = re.compile(r'Requests per second:\s+(\d+\.\d+)')
-RE_TRANSF_RATE = re.compile('Transfer rate:\s+(\d+\.\d+)')
 
-def dump_as_csv(r, f):
-  r = r[f]
-  f = file('benchmark-result-' + f + '.csv', 'w')
-  for key in sorted(r.keys()):
-    results = r[key]
-    f.write(', '.join([str(key), str(results['rps']), str(results['tr'])]))
-    f.write('\n')
-  f.close()
+def clearstderrline():
+  sys.stderr.write('\033[2K')
 
-if os.path.exists('benchmark.pickle'):
-  results = pickle.load(file('benchmark.pickle'))
-  analyzed = {'keep-alive':{}, 'close':{}}
-  for keep_alive, conns in results.keys():
-    ab_output = results[(keep_alive,conns)]
-    reqs_per_second = RE_RPS.findall(ab_output)
-    if not reqs_per_second:
-      continue
-    reqs_per_second = float(reqs_per_second[0])
-    
-    transf_rate = RE_TRANSF_RATE.findall(ab_output)
-    if not transf_rate:
-      continue
-    transf_rate = float(transf_rate[0])
-    
-    analyzed['keep-alive' if keep_alive == '-k' else 'close'][conns] = {
-      'rps': reqs_per_second,
-      'tr': transf_rate
-    }
+
+def weighttp(url, n_threads, n_connections, n_requests, keep_alive):
+  keep_alive = '-k' if keep_alive else ''
+  command = 'weighttp %(keep_alive)s ' \
+            '-t %(n_threads)d ' \
+            '-c %(n_connections)d ' \
+            '-n %(n_requests)d ' \
+            '-j ' \
+            '%(url)s 2> /dev/null' % locals()
+
+  clearstderrline()
+  sys.stderr.write('*** %s\r' % command)
+
+  output = commands.getoutput(command)
   
-  dump_as_csv(analyzed, 'keep-alive')
-  dump_as_csv(analyzed, 'close')
+  return json.loads(output)
 
-else:
-  NUM_REQUESTS = 80000
-  MAX_SIM_CONNECTIONS = 100
-  AB = '/usr/bin/ab'
-  ADDRESS = 'http://localhost:8080/hello'
 
-  results = {}
-  for keep_alive in ['', '-k']:
-    print 'Benchmarking', 'without' if keep_alive == '' else 'with', 'keep-alive connections'
-    for sim_connection in range(1, MAX_SIM_CONNECTIONS + 1):
-      print '    %d simultaneous connections, %d total requests' % (sim_connection, NUM_REQUESTS)
-      command_line = '%(ab)s %(keep_alive)s -i -n%(requests)d -c%(sim_connection)d %(url)s' % {
-        'ab': AB,
-        'keep_alive': keep_alive,
-        'sim_connection': sim_connection,
-        'requests': NUM_REQUESTS,
-        'url': ADDRESS
-      }
-      
-      results[(keep_alive,sim_connection)] = commands.getoutput(command_line)
+def steprange(initial, final, steps=10):
+  step = (final - initial) / steps
 
-  print 'Saving results to benchmark.pickle: run again to process it'
-  f = file('benchmark.pickle', 'w')
-  pickle.dump(results, f)
-  f.close()
+  while initial <= final:
+    yield initial
+    initial += step
 
+
+def sleepwithstatus(msg, period):
+  slept = 0
+  spinner = 0
+
+  while slept <= period:
+    clearstderrline()
+    sys.stderr.write('\r%s: %s' % (msg, '/|\\-'[spinner % 4]))
+
+    time.sleep(0.1)
+    slept += 0.1
+    spinner += 1
+
+  sys.stderr.write('\r')
+  clearstderrline()
+
+
+if __name__ == '__main__':
+  url = sys.argv[1] if len(sys.argv) > 1 else 'http://localhost:8080/100.html'
+  n_threads = 2
+  n_requests = 1000000
+  keep_alive_timeout = 5
+
+  print 'keep_alive,n_connections,rps,kbps,2xx,3xx,4xx,5xx'
+  for keep_alive in (True, False):
+    for n_connections in steprange(100, 60000, 10):
+      results = weighttp(url, n_threads, n_connections, n_requests, keep_alive)
+      status = results['status_codes']
+
+      clearstderrline()
+      print ','.join(str(token) for token in
+        (int(keep_alive), n_connections, results['reqs_per_sec'],
+         results['kbyte_per_sec'], status['2xx'], status['3xx'],
+         status['4xx'], status['5xx'])
+      )
+
+      sleepwithstatus('Waiting for keepalive connection timeout', keep_alive_timeout * 1.1)
+
+  clearstderrline()
