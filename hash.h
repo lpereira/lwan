a2b56338429c40318e47d7b2a9ca4526780a3d63
Leandro Pereira <leandro@hardinfo.org>
2013-09-12 08:43:10 -0300

    Remove hash_del_predicate(), as it's not being used anymore
    If that is ever needed again, it would be better to have an iterator with
    a predicate -- useful for both iterating outside the hash table and inside,
    with a deleting helper.

--- hash.h
+++ hash.h
@@ -19,9 +19,6 @@ void hash_free(struct hash *hash);
 int hash_add(struct hash *hash, const void *key, const void *value);
 int hash_add_unique(struct hash *hash, const void *key, const void *value);
 int hash_del(struct hash *hash, const void *key);
-void hash_del_predicate(struct hash *hash,
-			bool (*predicate)(const void *key, const size_t key_len, const void *data),
-			const void *data);
 void *hash_find(const struct hash *hash, const void *key);
 unsigned int hash_get_count(const struct hash *hash);
 void hash_iter_init(const struct hash *hash, struct hash_iter *iter);
