9ba3950e16320dd2bff9aaf01360b589eb048d8e
Leandro Pereira <leandro@hardinfo.org>
2014-01-12 10:30:27 -0200

    Smaller buffer for uint->string conversions

--- common/lwan-response.c
+++ common/lwan-response.c
@@ -237,7 +237,7 @@ lwan_prepare_response_header(lwan_request_t *request, lwan_http_status_t status,
 {
     char *p_headers;
     char *p_headers_end = headers + headers_buf_size;
-    char buffer[32];
+    char buffer[3 * sizeof(unsigned int)];
     size_t len;
 
     p_headers = headers;
