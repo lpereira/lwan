b71a193c30b4e4f5e1a1916966c6d61e4b992da5
Leandro Pereira <leandro@hardinfo.org>
2014-03-04 22:18:33 -0300

    Generate mime-types.h during build using public domain mime.types
    Closes #8.

--- tools/parse-mime-types.py
+++ tools/parse-mime-types.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python
 
+import sys
 import struct
 import zlib
+from operator import itemgetter
 
 known_exts = set()
 types = []
-for l in file('/etc/mime.types'):
+for l in open(sys.argv[1]):
   l = l.strip()
+  if l.startswith('#'):
+    continue
+  if '#' in l:
+    l = l.split('#')[0].strip()
   try:
     last_tab = l.rindex('\t')
   except ValueError:
@@ -18,46 +24,45 @@ for l in file('/etc/mime.types'):
     known_exts.add(extension)
     types.append((mime_type, extension))
 
-types.sort(lambda a, b: cmp(a[1], b[1]))
+types.sort(key = itemgetter(1))
 
 max_ext_len = max(len(ext) for typ, ext in types)
 max_typ_len = max(len(typ) for typ, ext in types)
 total_len = len(types) * (max_ext_len + 1 + max_typ_len + 1)
 
-out = ''
+out = b''
 entries = 0
 for typ, ext in types:
   entries += 1
-  out += struct.pack('%ds' % len(ext), ext)
+  out += struct.pack(str(len(ext)) + 's', bytes(ext, 'ascii'))
   out += struct.pack('b', 0)
-  out += struct.pack('%ds' % len(typ), typ)
+  out += struct.pack(str(len(typ)) + 's', bytes(typ, 'ascii'))
   out += struct.pack('b', 0)
 
 compressed_out = zlib.compress(out, 9)
 
-print '#ifndef __MIME_TYPES_H__'
-print '#define __MIME_TYPES_H__'
-print '/* Auto generated from parse-mime-types.py, do not modify */'
+print('#ifndef __MIME_TYPES_H__')
+print('#define __MIME_TYPES_H__')
+print('/* Auto generated from parse-mime-types.py, do not modify */')
 
-print '#define MIME_UNCOMPRESSED_LEN %d' % len(out)
-print '#define MIME_COMPRESSED_LEN %d' % len(compressed_out)
-print '#define MIME_ENTRIES %d' % entries
+print('#define MIME_UNCOMPRESSED_LEN', len(out))
+print('#define MIME_COMPRESSED_LEN', len(compressed_out))
+print('#define MIME_ENTRIES', entries)
 
-print 'struct mime_entry {'
-print '  const char *extension;'
-print '  const char *type;'
-print '};'
+print('struct mime_entry {')
+print('  const char *extension;')
+print('  const char *type;')
+print('};')
 
-print 'static const unsigned char mime_entries_compressed[] = {'
+print('static const unsigned char mime_entries_compressed[] = {')
 for index, b in enumerate(compressed_out):
   if index > 0 and index % 13 == 0:
-    print ''
+    print()
 
-  b = ord(b)
   if b < 0x10:
-    print '0x0%x,' % b,
+    print('0x0%x,' % b, end=' ')
   else:
-    print '0x%x,' % b,
-print '};'
+    print('0x%x,' % b, end=' ')
+print('};')
 
-print '#endif  /* __MIME_TYPES_H__ */'
+print('#endif  /* __MIME_TYPES_H__ */')
