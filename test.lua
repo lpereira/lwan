9e6c320dba2b04692fc8637276f01c14a63971ea
Leandro Pereira <leandro@hardinfo.org>
2014-11-24 21:15:10 -0200

    Use 4-space indent in test.lua

--- test.lua
+++ test.lua
@@ -1,20 +1,20 @@
 function handle_get_hello(req)
-	local name = req:query_param[[name]]
-	if name then
-		req:set_response("Hello, " .. name .. "!")
-	else
-		req:set_response("Hello, World!")
-	end
+    local name = req:query_param[[name]]
+    if name then
+        req:set_response("Hello, " .. name .. "!")
+    else
+        req:set_response("Hello, World!")
+    end
 end
 
 function handle_get_chunked(req)
-	for i = 0, 10 do
-		req:say("Chunk #" .. i .. "\n")
-	end
+    for i = 0, 10 do
+        req:say("Chunk #" .. i .. "\n")
+    end
 end
 
 function handle_get_random(req)
-	req:set_response("Random number: " .. math.random())
+    req:set_response("Random number: " .. math.random())
 end
 
 function string.starts(String, Start)
@@ -23,9 +23,9 @@ function string.starts(String, Start)
 end
 
 function handle_get_root(req)
-	for key, value in pairs(_G) do
-		if string.starts(key, "handle_get_") and type(value) == "function" then
-			req:say("<li><a href=" .. string.sub(key, 12) .. ">" .. key .. "</a></li>\n")
-		end
-	end	
+    for key, value in pairs(_G) do
+        if string.starts(key, "handle_get_") and type(value) == "function" then
+            req:say("<li><a href=" .. string.sub(key, 12) .. ">" .. key .. "</a></li>\n")
+        end
+    end
 end
