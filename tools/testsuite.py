67061dd2853dc6af08d9a08a5ca0ca5459b77cba
Malthe Borch <mborch@gmail.com>
2015-10-20 14:12:20 +0200

    PEP8

--- tools/testsuite.py
+++ tools/testsuite.py
@@ -23,9 +23,10 @@ print 'Using', LWAN_PATH, 'for lwan'
 class LwanTest(unittest.TestCase):
   def setUp(self):
     for spawn_try in range(20):
-      self.lwan=subprocess.Popen([LWAN_PATH],
-            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
+      self.lwan=subprocess.Popen(
+        [LWAN_PATH],
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+      )
       for request_try in range(20):
         try:
           requests.get('http://127.0.0.1:8080/hello')
