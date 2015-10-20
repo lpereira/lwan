e3464b41511322a416b5a6a2691448f6c6a0a8bc
Malthe Borch <mborch@gmail.com>
2015-10-20 14:12:39 +0200

    Wait until port is open, no need to make a request

--- tools/testsuite.py
+++ tools/testsuite.py
@@ -27,12 +27,15 @@ class LwanTest(unittest.TestCase):
         [LWAN_PATH],
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT
       )
+      s = socket.socket()
       for request_try in range(20):
         try:
-          requests.get('http://127.0.0.1:8080/hello')
-          return
-        except requests.ConnectionError:
+          s.connect(('127.0.0.1', 8080))
+        except:
           time.sleep(0.1)
+        else:
+          s.close()
+          return
 
       time.sleep(0.1)
 
