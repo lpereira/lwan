6f3e97a41e8d9524dbafc7fd6d6ac18e158a44d7
Leandro Pereira <leandro@hardinfo.org>
2016-02-21 14:27:34 -0300

    No need for testrunner to have command line arguments

--- testrunner/main.c
+++ testrunner/main.c
@@ -18,19 +18,11 @@
  */
 
 #define _GNU_SOURCE
-#include <getopt.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
 
 #include "lwan.h"
 #include "lwan-serve-files.h"
-
-enum args {
-  ARGS_FAILED,
-  ARGS_USE_CONFIG,
-  ARGS_SERVE_FILES
-};
 
 lwan_http_status_t
 gif_beacon(lwan_request_t *request __attribute__((unused)),
@@ -169,88 +161,12 @@ end:
     return HTTP_OK;
 }
 
-static enum args
-parse_args(int argc, char *argv[], lwan_config_t *config, char *root)
-{
-  static const struct option opts[] = {
-    { .name = "root", .has_arg = 1, .val = 'r' },
-    { .name = "listen", .has_arg = 1, .val = 'l' },
-    { .name = "help", .val = 'h' },
-    { }
-  };
-  int c, optidx = 0;
-  enum args result = ARGS_USE_CONFIG;
-
-  while ((c = getopt_long(argc, argv, "hr:l:", opts, &optidx)) != -1) {
-    switch (c) {
-    case 'l':
-      free(config->listener);
-      config->listener = strdup(optarg);
-      result = ARGS_SERVE_FILES;
-      break;
-
-    case 'r':
-      memcpy(root, optarg, strnlen(optarg, PATH_MAX - 1) + 1);
-      result = ARGS_SERVE_FILES;
-      break;
-
-    default:
-      printf("Run %s --help for usage information.\n", argv[0]);
-      return ARGS_FAILED;
-
-    case 'h':
-      printf("Usage: %s [--root /path/to/root/dir] [--listener addr:port]\n", argv[0]);
-      printf("\t[--config]\n");
-      printf("Serve files through HTTP.\n\n");
-      printf("Defaults to listening on all interfaces, port 8080, serving current directory.\n\n");
-      printf("Options:\n");
-      printf("\t-r, --root      Path to serve files from (default: current dir).\n");
-      printf("\t-l, --listener  Listener (default: %s).\n", config->listener);
-      printf("\t-h, --help      This.\n");
-      printf("\n");
-      printf("Examples:\n");
-      printf("  Serve system-wide documentation: %s -r /usr/share/doc\n", argv[0]);
-      printf("        Serve on a different port: %s -l '*:1337'\n", argv[0]);
-      printf("\n");
-      printf("Report bugs at <https://github.com/lpereira/lwan>.\n");
-      return ARGS_FAILED;
-    }
-  }
-
-  return result;
-}
-
 int
-main(int argc, char *argv[])
+main()
 {
     lwan_t l;
-    lwan_config_t c;
-    char root[PATH_MAX];
 
-    if (!getcwd(root, PATH_MAX))
-        return 1;
-
-    c = *lwan_get_default_config();
-    c.listener = strdup("*:8080");
-
-    switch (parse_args(argc, argv, &c, root)) {
-    case ARGS_SERVE_FILES:
-        lwan_status_info("Serving files from %s", root);
-        lwan_init_with_config(&l, &c);
-
-        const lwan_url_map_t map[] = {
-            { .prefix = "/", SERVE_FILES(root) },
-            { }
-        };
-        lwan_set_url_map(&l, map);
-        break;
-    case ARGS_USE_CONFIG:
-        lwan_init(&l);
-        break;
-    case ARGS_FAILED:
-        return EXIT_FAILURE;
-    }
-
+    lwan_init(&l);
     lwan_main_loop(&l);
     lwan_shutdown(&l);
 
