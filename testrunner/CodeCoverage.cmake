f56d4e0af61d1265ba3b325ef1968f000290e79e
Leandro Pereira <leandro@hardinfo.org>
2016-08-03 20:41:49 -0300

    Ignore errors while generating Coverage reports

--- testrunner/CodeCoverage.cmake
+++ testrunner/CodeCoverage.cmake
@@ -144,7 +144,7 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 		COMMAND ${LCOV_PATH} --directory . --zerocounters
 
 		# Run tests
-		COMMAND ${test_command} ${ARGV3}
+		COMMAND ${test_command} ${ARGV3} || exit 0
 
 		# Capturing lcov counters and generating report
 		COMMAND ${LCOV_PATH} --directory . --capture --output-file ${coverage_info}
