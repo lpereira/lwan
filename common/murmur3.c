bd3b2e5401d014adc818af29293208f0c28187c1
Leandro Pereira <leandro@hardinfo.org>
2017-07-12 18:49:06 -0700

    Fix warnings in murmur3 when building with GCC7

--- common/murmur3.c
+++ common/murmur3.c
@@ -101,8 +101,10 @@ MurmurHash3_x86_32(const void *key, size_t len, uint32_t seed, void *out)
     switch (len & 3) {
     case 3:
         k1 ^= (uint32_t)tail[2] << 16;
+        /* fallthrough */
     case 2:
         k1 ^= (uint32_t)tail[1] << 8;
+        /* fallthrough */
     case 1:
         k1 ^= (uint32_t)tail[0];
         k1 *= c1;
@@ -171,26 +173,26 @@ FORCE_INLINE static void MurmurHash3_x86_128 (const void *key, const size_t len,
     uint32_t k4 = 0;
 
     switch(len & 15) {
-    case 15: k4 ^= (uint32_t)tail[14] << 16;
-    case 14: k4 ^= (uint32_t)tail[13] << 8;
+    case 15: k4 ^= (uint32_t)tail[14] << 16; /* fallthrough */
+    case 14: k4 ^= (uint32_t)tail[13] << 8;  /* fallthrough */
     case 13: k4 ^= (uint32_t)tail[12] << 0;
              k4 *= c4; k4 = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
-
-    case 12: k3 ^= (uint32_t)tail[11] << 24;
-    case 11: k3 ^= (uint32_t)tail[10] << 16;
-    case 10: k3 ^= (uint32_t)tail[ 9] << 8;
+             /* fallthrough */
+    case 12: k3 ^= (uint32_t)tail[11] << 24; /* fallthrough */
+    case 11: k3 ^= (uint32_t)tail[10] << 16; /* fallthrough */
+    case 10: k3 ^= (uint32_t)tail[ 9] << 8;  /* fallthrough */
     case  9: k3 ^= (uint32_t)tail[ 8] << 0;
              k3 *= c3; k3 = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
-
-    case  8: k2 ^= (uint32_t)tail[ 7] << 24;
-    case  7: k2 ^= (uint32_t)tail[ 6] << 16;
-    case  6: k2 ^= (uint32_t)tail[ 5] << 8;
+             /* fallthrough */
+    case  8: k2 ^= (uint32_t)tail[ 7] << 24; /* fallthrough */
+    case  7: k2 ^= (uint32_t)tail[ 6] << 16; /* fallthrough */
+    case  6: k2 ^= (uint32_t)tail[ 5] << 8;  /* fallthrough */
     case  5: k2 ^= (uint32_t)tail[ 4] << 0;
              k2 *= c2; k2 = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
-
-    case  4: k1 ^= (uint32_t)tail[ 3] << 24;
-    case  3: k1 ^= (uint32_t)tail[ 2] << 16;
-    case  2: k1 ^= (uint32_t)tail[ 1] << 8;
+             /* fallthrough */
+    case  4: k1 ^= (uint32_t)tail[ 3] << 24; /* fallthrough */
+    case  3: k1 ^= (uint32_t)tail[ 2] << 16; /* fallthrough */
+    case  2: k1 ^= (uint32_t)tail[ 1] << 8;  /* fallthrough */
     case  1: k1 ^= (uint32_t)tail[ 0] << 0;
              k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
     }
@@ -264,37 +266,45 @@ MurmurHash3_x64_128(const void *key, const size_t len, const uint32_t seed,
     uint64_t k2 = 0;
     switch (len & 15) {
     case 15:
-        k2 ^= (uint64_t) (tail[14]) << 48;
+        k2 ^= (uint64_t) (tail[14]) << 48; /* fallthrough */
     case 14:
-        k2 ^= (uint64_t) (tail[13]) << 40;
+        k2 ^= (uint64_t) (tail[13]) << 40; /* fallthrough */
     case 13:
-        k2 ^= (uint64_t) (tail[12]) << 32;
+        k2 ^= (uint64_t) (tail[12]) << 32; /* fallthrough */
     case 12:
-        k2 ^= (uint64_t) (tail[11]) << 24;
+        k2 ^= (uint64_t) (tail[11]) << 24; /* fallthrough */
     case 11:
-        k2 ^= (uint64_t) (tail[10]) << 16;
+        k2 ^= (uint64_t) (tail[10]) << 16; /* fallthrough */
     case 10:
-        k2 ^= (uint64_t) (tail[9]) << 8;
+        k2 ^= (uint64_t) (tail[9]) << 8; /* fallthrough */
     case 9:
         k2 ^= (uint64_t) (tail[8]) << 0;
         k2 *= c2;
         k2 = ROTL64(k2, 33);
         k2 *= c1;
         h2 ^= k2;
+        /* fallthrough */
     case 8:
         k1 ^= (uint64_t) (tail[7]) << 56;
+        /* fallthrough */
     case 7:
         k1 ^= (uint64_t) (tail[6]) << 48;
+        /* fallthrough */
     case 6:
         k1 ^= (uint64_t) (tail[5]) << 40;
+        /* fallthrough */
     case 5:
         k1 ^= (uint64_t) (tail[4]) << 32;
+        /* fallthrough */
     case 4:
         k1 ^= (uint64_t) (tail[3]) << 24;
+        /* fallthrough */
     case 3:
         k1 ^= (uint64_t) (tail[2]) << 16;
+        /* fallthrough */
     case 2:
         k1 ^= (uint64_t) (tail[1]) << 8;
+        /* fallthrough */
     case 1:
         k1 ^= (uint64_t) (tail[0]) << 0;
         k1 *= c1;
