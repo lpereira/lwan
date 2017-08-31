bd2b93c9f7eab502b953d8c599a98d7df006aa4a
Leandro Pereira <leandro@hardinfo.org>
2017-08-31 07:42:42 -0700

    Fix compilation warnings when building with Clang
    If -Wunused-functions is set, GCC won't display warnings for static
    inline functions inside headers if they end up not being used. But
    Clang will issue a warning.  Mark the array functions generated by
    the DEFINE_ARRAY_TYPE() macro with __attribute__((unused)) to
    silence these warnings.

--- common/lwan-array.h
+++ common/lwan-array.h
@@ -38,22 +38,27 @@ struct lwan_array *coro_lwan_array_new(struct coro *coro);
     struct array_type_ { \
         struct lwan_array base; \
     }; \
+    __attribute__((unused)) \
     static inline int array_type_ ## _init(struct array_type_ *array) \
     { \
         return lwan_array_init((struct lwan_array *)array); \
     } \
+    __attribute__((unused)) \
     static inline int array_type_ ## _reset(struct array_type_ *array) \
     { \
         return lwan_array_reset((struct lwan_array *)array); \
     } \
+    __attribute__((unused)) \
     static inline element_type_ * array_type_ ## _append(struct array_type_ *array) \
     { \
         return (element_type_ *)lwan_array_append((struct lwan_array *)array, sizeof(element_type_)); \
     } \
+    __attribute__((unused)) \
     static inline void array_type_ ## _sort(struct array_type_ *array, int (*cmp)(const void *a, const void *b)) \
     { \
         lwan_array_sort((struct lwan_array *)array, sizeof(element_type_), cmp); \
     } \
+    __attribute__((unused)) \
     static inline struct array_type_ *coro_ ## array_type_ ## _new(struct coro *coro) \
     { \
         return (struct array_type_ *)coro_lwan_array_new(coro); \
