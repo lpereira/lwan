6c1589d71da47cd753d9f8fe40783c35d3994a5a
Leandro Pereira <leandro@hardinfo.org>
2018-03-13 08:04:37 -0700

    Reindent lwan-socket

--- src/lib/lwan-socket.c
+++ src/lib/lwan-socket.c
@@ -14,7 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  */
 
 #define _GNU_SOURCE
@@ -31,11 +32,10 @@
 
 #include "lwan-private.h"
 
-#include "sd-daemon.h"
 #include "int-to-str.h"
+#include "sd-daemon.h"
 
-static int
-get_backlog_size(void)
+static int get_backlog_size(void)
 {
 #ifdef SOMAXCONN
     int backlog = SOMAXCONN;
@@ -55,14 +55,13 @@ get_backlog_size(void)
     return backlog;
 }
 
-static int
-setup_socket_from_systemd(void)
+static int setup_socket_from_systemd(void)
 {
     int fd = SD_LISTEN_FDS_START;
 
     if (!sd_is_socket_inet(fd, AF_UNSPEC, SOCK_STREAM, 1, 0))
         lwan_status_critical("Passed file descriptor is not a "
-            "listening TCP socket");
+                             "listening TCP socket");
 
     int flags = fcntl(fd, F_GETFD);
     if (flags < 0)
@@ -73,8 +72,7 @@ setup_socket_from_systemd(void)
     return fd;
 }
 
-static sa_family_t
-parse_listener_ipv4(char *listener, char **node, char **port)
+static sa_family_t parse_listener_ipv4(char *listener, char **node, char **port)
 {
     char *colon = strrchr(listener, ':');
     if (!colon) {
@@ -104,8 +102,7 @@ parse_listener_ipv4(char *listener, char **node, char **port)
     return AF_INET;
 }
 
-static sa_family_t
-parse_listener_ipv6(char *listener, char **node, char **port)
+static sa_family_t parse_listener_ipv6(char *listener, char **node, char **port)
 {
     char *last_colon = strrchr(listener, ':');
     if (!last_colon)
@@ -126,11 +123,11 @@ parse_listener_ipv6(char *listener, char **node, char **port)
     return AF_INET6;
 }
 
-static sa_family_t
-parse_listener(char *listener, char **node, char **port)
+static sa_family_t parse_listener(char *listener, char **node, char **port)
 {
     if (streq(listener, "systemd")) {
-        lwan_status_critical("Listener configured to use systemd socket activation, "
+        lwan_status_critical(
+            "Listener configured to use systemd socket activation, "
             "but started outside systemd.");
         return AF_UNSPEC;
     }
@@ -141,15 +138,15 @@ parse_listener(char *listener, char **node, char **port)
     return parse_listener_ipv4(listener, node, port);
 }
 
-static int
-listen_addrinfo(int fd, const struct addrinfo *addr)
+static int listen_addrinfo(int fd, const struct addrinfo *addr)
 {
     if (listen(fd, get_backlog_size()) < 0)
         lwan_status_critical_perror("listen");
 
     char host_buf[NI_MAXHOST], serv_buf[NI_MAXSERV];
-    int ret = getnameinfo(addr->ai_addr, addr->ai_addrlen, host_buf, sizeof(host_buf),
-                      serv_buf, sizeof(serv_buf), NI_NUMERICHOST | NI_NUMERICSERV);
+    int ret = getnameinfo(addr->ai_addr, addr->ai_addrlen, host_buf,
+                          sizeof(host_buf), serv_buf, sizeof(serv_buf),
+                          NI_NUMERICHOST | NI_NUMERICSERV);
     if (ret)
         lwan_status_critical("getnameinfo: %s", gai_strerror(ret));
 
@@ -161,38 +158,36 @@ listen_addrinfo(int fd, const struct addrinfo *addr)
     return fd;
 }
 
-#define SET_SOCKET_OPTION(_domain,_option,_param,_size) \
-    do { \
-        if (setsockopt(fd, (_domain), (_option), (_param), (_size)) < 0) \
-            lwan_status_critical_perror("setsockopt"); \
-    } while(0)
+#define SET_SOCKET_OPTION(_domain, _option, _param, _size)                     \
+    do {                                                                       \
+        if (setsockopt(fd, (_domain), (_option), (_param), (_size)) < 0)       \
+            lwan_status_critical_perror("setsockopt");                         \
+    } while (0)
 
-#define SET_SOCKET_OPTION_MAY_FAIL(_domain,_option,_param,_size) \
-    do { \
-        if (setsockopt(fd, (_domain), (_option), (_param), (_size)) < 0) \
-            lwan_status_warning("%s not supported by the kernel", \
-                #_option); \
-    } while(0)
+#define SET_SOCKET_OPTION_MAY_FAIL(_domain, _option, _param, _size)            \
+    do {                                                                       \
+        if (setsockopt(fd, (_domain), (_option), (_param), (_size)) < 0)       \
+            lwan_status_warning("%s not supported by the kernel", #_option);   \
+    } while (0)
 
 #ifndef SO_REUSEPORT
 #define SO_REUSEPORT 15
 #endif
 
-static int
-bind_and_listen_addrinfos(struct addrinfo *addrs, bool reuse_port)
+static int bind_and_listen_addrinfos(struct addrinfo *addrs, bool reuse_port)
 {
     const struct addrinfo *addr;
 
     /* Try each address until we bind one successfully. */
     for (addr = addrs; addr; addr = addr->ai_next) {
-        int fd = socket(addr->ai_family,
-            addr->ai_socktype | SOCK_CLOEXEC, addr->ai_protocol);
+        int fd = socket(addr->ai_family, addr->ai_socktype | SOCK_CLOEXEC,
+                        addr->ai_protocol);
         if (fd < 0)
             continue;
 
-        SET_SOCKET_OPTION(SOL_SOCKET, SO_REUSEADDR, (int[]){ 1 }, sizeof(int));
+        SET_SOCKET_OPTION(SOL_SOCKET, SO_REUSEADDR, (int[]){1}, sizeof(int));
         SET_SOCKET_OPTION_MAY_FAIL(SOL_SOCKET, SO_REUSEPORT,
-                                                (int[]){ reuse_port }, sizeof(int));
+                                   (int[]){reuse_port}, sizeof(int));
 
         if (!bind(fd, addr->ai_addr, addr->ai_addrlen))
             return listen_addrinfo(fd, addr);
@@ -203,21 +198,19 @@ bind_and_listen_addrinfos(struct addrinfo *addrs, bool reuse_port)
     lwan_status_critical("Could not bind socket");
 }
 
-static int
-setup_socket_normally(struct lwan *l)
+static int setup_socket_normally(struct lwan *l)
 {
     char *node, *port;
     char *listener = strdupa(l->config.listener);
     sa_family_t family = parse_listener(listener, &node, &port);
     if (family == AF_UNSPEC)
-        lwan_status_critical("Could not parse listener: %s", l->config.listener);
+        lwan_status_critical("Could not parse listener: %s",
+                             l->config.listener);
 
     struct addrinfo *addrs;
-    struct addrinfo hints = {
-        .ai_family = family,
-        .ai_socktype = SOCK_STREAM,
-        .ai_flags = AI_PASSIVE
-    };
+    struct addrinfo hints = {.ai_family = family,
+                             .ai_socktype = SOCK_STREAM,
+                             .ai_flags = AI_PASSIVE};
 
     int ret = getaddrinfo(node, port, &hints, &addrs);
     if (ret)
@@ -232,8 +225,7 @@ setup_socket_normally(struct lwan *l)
 #define TCP_FASTOPEN 23
 #endif
 
-void
-lwan_socket_init(struct lwan *l)
+void lwan_socket_init(struct lwan *l)
 {
     int fd, n;
 
@@ -249,13 +241,12 @@ lwan_socket_init(struct lwan *l)
     }
 
     SET_SOCKET_OPTION(SOL_SOCKET, SO_LINGER,
-        (&(struct linger){ .l_onoff = 1, .l_linger = 1 }), sizeof(struct linger));
+                      (&(struct linger){.l_onoff = 1, .l_linger = 1}),
+                      sizeof(struct linger));
 
 #ifdef __linux__
-    SET_SOCKET_OPTION_MAY_FAIL(SOL_TCP, TCP_FASTOPEN,
-                                            (int[]){ 5 }, sizeof(int));
-    SET_SOCKET_OPTION_MAY_FAIL(SOL_TCP, TCP_QUICKACK,
-                                            (int[]){ 0 }, sizeof(int));
+    SET_SOCKET_OPTION_MAY_FAIL(SOL_TCP, TCP_FASTOPEN, (int[]){5}, sizeof(int));
+    SET_SOCKET_OPTION_MAY_FAIL(SOL_TCP, TCP_QUICKACK, (int[]){0}, sizeof(int));
 #endif
 
     l->main_socket = fd;
