80b37e865a556f50ff2ba2e6d4cde17819b45bdc
Leandro Pereira <leandro@hardinfo.org>
2018-05-19 10:39:53 -0700

    Reindent list.c

--- src/lib/list.c
+++ src/lib/list.c
@@ -33,40 +33,39 @@
 #include "list.h"
 
 static void *corrupt(const char *abortstr,
-		     const struct list_node *head,
-		     const struct list_node *node,
-		     unsigned int count)
+                     const struct list_node *head,
+                     const struct list_node *node,
+                     unsigned int count)
 {
-	if (abortstr) {
-		fprintf(stderr,
-			"%s: prev corrupt in node %p (%u) of %p\n",
-			abortstr, node, count, head);
-		abort();
-	}
-	return NULL;
+    if (abortstr) {
+        fprintf(stderr, "%s: prev corrupt in node %p (%u) of %p\n", abortstr,
+                node, count, head);
+        abort();
+    }
+    return NULL;
 }
 
 struct list_node *list_check_node(const struct list_node *node,
-				  const char *abortstr)
+                                  const char *abortstr)
 {
-	const struct list_node *p, *n;
-	unsigned int count = 0;
+    const struct list_node *p, *n;
+    unsigned int count = 0;
 
-	for (p = node, n = node->next; n != node; p = n, n = n->next) {
-		count++;
-		if (n->prev != p)
-			return corrupt(abortstr, node, n, count);
-	}
-	/* Check prev on head node. */
-	if (node->prev != p)
-		return corrupt(abortstr, node, node, 0);
+    for (p = node, n = node->next; n != node; p = n, n = n->next) {
+        count++;
+        if (n->prev != p)
+            return corrupt(abortstr, node, n, count);
+    }
+    /* Check prev on head node. */
+    if (node->prev != p)
+        return corrupt(abortstr, node, node, 0);
 
-	return (struct list_node *)node;
+    return (struct list_node *)node;
 }
 
 struct list_head *list_check(const struct list_head *h, const char *abortstr)
 {
-	if (!list_check_node(&h->n, abortstr))
-		return NULL;
-	return (struct list_head *)h;
+    if (!list_check_node(&h->n, abortstr))
+        return NULL;
+    return (struct list_head *)h;
 }
