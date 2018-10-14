f927ac634497d60658d89e9edd24c474e2818fe1
Leandro Pereira <leandro@hardinfo.org>
2018-10-14 16:36:55 -0700

    Revert "Reduce number of config_line copies per call to config_read_line()"
    This reverts commit c22a60fcf5358e3f613d87d99e82cf3401dda11d.

--- src/lib/lwan-config.c
+++ src/lib/lwan-config.c
@@ -708,10 +708,17 @@ void config_close(struct config *config)
 
 bool config_read_line(struct config *conf, struct config_line *cl)
 {
+    struct config_line *ptr;
+    bool ret;
+
     if (conf->error_message)
         return false;
 
-    return parser_next(&conf->parser, &cl);
+    ret = parser_next(&conf->parser, &ptr);
+    if (ret)
+        *cl = *ptr;
+
+    return ret;
 }
 
 static bool find_section_end(struct config *config)
