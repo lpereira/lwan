1bd8d1243f03f9e4cf304b2df9df82cbd7b4e054
Leandro Pereira <leandro@hardinfo.org>
2018-10-14 08:09:47 -0700

    Reindent lwan.h

--- src/lib/lwan.h
+++ src/lib/lwan.h
@@ -14,30 +14,31 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  */
 
 #pragma once
 
-#if defined (__cplusplus)
+#if defined(__cplusplus)
 extern "C" {
 #endif
 
+#include <netinet/in.h>
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-#include <netinet/in.h>
 
 #include "hash.h"
+#include "queue.h"
+#include "timeout.h"
 #include "lwan-array.h"
 #include "lwan-config.h"
 #include "lwan-coro.h"
 #include "lwan-status.h"
-#include "lwan-trie.h"
 #include "lwan-strbuf.h"
-#include "queue.h"
-#include "timeout.h"
+#include "lwan-trie.h"
 
 #define DEFAULT_BUFFER_SIZE 4096
 #define DEFAULT_HEADERS_SIZE 512
@@ -50,15 +51,15 @@ extern "C" {
     extern const struct lwan_module_info lwan_module_info_##name_;
 
 #ifdef __APPLE__
-#  define LWAN_SECTION_NAME(name_) "__DATA," # name_
+#define LWAN_SECTION_NAME(name_) "__DATA," #name_
 #else
-#  define LWAN_SECTION_NAME(name_) #name_
+#define LWAN_SECTION_NAME(name_) #name_
 #endif
 
 #define LWAN_REGISTER_MODULE(name_, module_)                                   \
-  const struct lwan_module_info                                                \
-      __attribute__((used, section(LWAN_SECTION_NAME(lwan_module))))           \
-          lwan_module_info_##name_ = {.name = #name_, .module = module_}
+    const struct lwan_module_info                                              \
+        __attribute__((used, section(LWAN_SECTION_NAME(lwan_module))))         \
+            lwan_module_info_##name_ = {.name = #name_, .module = module_}
 
 #define LWAN_HANDLER_REF(name_) lwan_handler_##name_
 
@@ -81,41 +82,41 @@ extern "C" {
     LWAN_HANDLER_DEFINE(name_)
 
 #ifdef DISABLE_INLINE_FUNCTIONS
-#  define ALWAYS_INLINE
+#define ALWAYS_INLINE
 #else
-#  define ALWAYS_INLINE inline __attribute__((always_inline))
+#define ALWAYS_INLINE inline __attribute__((always_inline))
 #endif
 
 #ifdef DISABLE_BRANCH_PREDICTION
-#  define LIKELY_IS(x,y) (x)
+#define LIKELY_IS(x, y) (x)
 #else
-#  define LIKELY_IS(x,y)	__builtin_expect((x), (y))
+#define LIKELY_IS(x, y) __builtin_expect((x), (y))
 #endif
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#  define MULTICHAR_CONSTANT(a,b,c,d) \
+#define MULTICHAR_CONSTANT(a, b, c, d)                                         \
     ((int32_t)((a) | (b) << 8 | (c) << 16 | (d) << 24))
-#  define MULTICHAR_CONSTANT_SMALL(a,b) \
-    ((int16_t)((a) | (b) << 8))
-#  define MULTICHAR_CONSTANT_LARGE(a,b,c,d,e,f,g,h) \
-    ((int64_t)MULTICHAR_CONSTANT(a,b,c,d) | (int64_t)MULTICHAR_CONSTANT(e,f,g,h)<<32)
+#define MULTICHAR_CONSTANT_SMALL(a, b) ((int16_t)((a) | (b) << 8))
+#define MULTICHAR_CONSTANT_LARGE(a, b, c, d, e, f, g, h)                       \
+    ((int64_t)MULTICHAR_CONSTANT(a, b, c, d) |                                 \
+     (int64_t)MULTICHAR_CONSTANT(e, f, g, h) << 32)
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-#  define MULTICHAR_CONSTANT(d,c,b,a) \
+#define MULTICHAR_CONSTANT(d, c, b, a)                                         \
     ((int32_t)((a) | (b) << 8 | (c) << 16 | (d) << 24))
-#  define MULTICHAR_CONSTANT_SMALL(b,a) \
-    ((int16_t)((a) | (b) << 8))
-#  define MULTICHAR_CONSTANT_LARGE(a,b,c,d,e,f,g,h) \
-    ((int64_t)MULTICHAR_CONSTANT(a,b,c,d)<<32 | (int64_t)MULTICHAR_CONSTANT(e,f,g,h))
+#define MULTICHAR_CONSTANT_SMALL(b, a) ((int16_t)((a) | (b) << 8))
+#define MULTICHAR_CONSTANT_LARGE(a, b, c, d, e, f, g, h)                       \
+    ((int64_t)MULTICHAR_CONSTANT(a, b, c, d) << 32 |                           \
+     (int64_t)MULTICHAR_CONSTANT(e, f, g, h))
 #elif __BYTE_ORDER__ == __ORDER_PDP_ENDIAN__
-#  error A PDP? Seriously?
+#error A PDP? Seriously?
 #endif
 
-#define MULTICHAR_CONSTANT_L(a,b,c,d) \
-    (MULTICHAR_CONSTANT(a,b,c,d) | 0x20202020)
-#define MULTICHAR_CONSTANT_SMALL_L(a,b) \
-    (MULTICHAR_CONSTANT_SMALL(a,b) | 0x2020)
-#define MULTICHAR_CONSTANT_LARGE_L(a,b,c,d,e,f,g,h) \
-    (MULTICHAR_CONSTANT_LARGE(a,b,c,d,e,f,g,h) | 0x2020202020202020)
+#define MULTICHAR_CONSTANT_L(a, b, c, d)                                       \
+    (MULTICHAR_CONSTANT(a, b, c, d) | 0x20202020)
+#define MULTICHAR_CONSTANT_SMALL_L(a, b)                                       \
+    (MULTICHAR_CONSTANT_SMALL(a, b) | 0x2020)
+#define MULTICHAR_CONSTANT_LARGE_L(a, b, c, d, e, f, g, h)                     \
+    (MULTICHAR_CONSTANT_LARGE(a, b, c, d, e, f, g, h) | 0x2020202020202020)
 
 static ALWAYS_INLINE int64_t string_as_int64(const char *s)
 {
@@ -145,21 +146,22 @@ static ALWAYS_INLINE int16_t string_as_int16(const char *s)
 #define STRING_SWITCH_SMALL_L(s) switch (string_as_int16(s) | 0x2020)
 
 #define STRING_SWITCH_LARGE(s) switch (string_as_int64(s))
-#define STRING_SWITCH_LARGE_L(s) switch (string_as_int64(s) | 0x2020202020202020)
+#define STRING_SWITCH_LARGE_L(s)                                               \
+    switch (string_as_int64(s) | 0x2020202020202020)
 
-#define LIKELY(x)	LIKELY_IS(!!(x), 1)
-#define UNLIKELY(x)	LIKELY_IS((x), 0)
+#define LIKELY(x) LIKELY_IS(!!(x), 1)
+#define UNLIKELY(x) LIKELY_IS((x), 0)
 
-#define ATOMIC_READ(V)		(*(volatile typeof(V) *)&(V))
-#define ATOMIC_AAF(P, V) 	(__sync_add_and_fetch((P), (V)))
-#define ATOMIC_INC(V)		ATOMIC_AAF(&(V), 1)
-#define ATOMIC_DEC(V)		ATOMIC_AAF(&(V), -1)
+#define ATOMIC_READ(V) (*(volatile typeof(V) *)&(V))
+#define ATOMIC_AAF(P, V) (__sync_add_and_fetch((P), (V)))
+#define ATOMIC_INC(V) ATOMIC_AAF(&(V), 1)
+#define ATOMIC_DEC(V) ATOMIC_AAF(&(V), -1)
 #define ATOMIC_BITWISE(P, O, V) (__sync_##O##_and_fetch((P), (V)))
 
-#if defined (__cplusplus)
+#if defined(__cplusplus)
 #define ENFORCE_STATIC_BUFFER_LENGTH
 #else
-#define ENFORCE_STATIC_BUFFER_LENGTH	static
+#define ENFORCE_STATIC_BUFFER_LENGTH static
 #endif
 
 enum lwan_http_status {
@@ -185,22 +187,22 @@ enum lwan_http_status {
 };
 
 enum lwan_handler_flags {
-    HANDLER_PARSE_QUERY_STRING = 1<<0,
-    HANDLER_PARSE_IF_MODIFIED_SINCE = 1<<1,
-    HANDLER_PARSE_RANGE = 1<<2,
-    HANDLER_PARSE_ACCEPT_ENCODING = 1<<3,
-    HANDLER_PARSE_POST_DATA = 1<<4,
-    HANDLER_MUST_AUTHORIZE = 1<<5,
-    HANDLER_REMOVE_LEADING_SLASH = 1<<6,
-    HANDLER_CAN_REWRITE_URL = 1<<7,
-    HANDLER_PARSE_COOKIES = 1<<8,
-    HANDLER_DATA_IS_HASH_TABLE = 1<<9,
+    HANDLER_PARSE_QUERY_STRING = 1 << 0,
+    HANDLER_PARSE_IF_MODIFIED_SINCE = 1 << 1,
+    HANDLER_PARSE_RANGE = 1 << 2,
+    HANDLER_PARSE_ACCEPT_ENCODING = 1 << 3,
+    HANDLER_PARSE_POST_DATA = 1 << 4,
+    HANDLER_MUST_AUTHORIZE = 1 << 5,
+    HANDLER_REMOVE_LEADING_SLASH = 1 << 6,
+    HANDLER_CAN_REWRITE_URL = 1 << 7,
+    HANDLER_PARSE_COOKIES = 1 << 8,
+    HANDLER_DATA_IS_HASH_TABLE = 1 << 9,
 
-    HANDLER_PARSE_MASK = 1<<0 | 1<<1 | 1<<2 | 1<<3 | 1<<4 | 1<<8
+    HANDLER_PARSE_MASK = 1 << 0 | 1 << 1 | 1 << 2 | 1 << 3 | 1 << 4 | 1 << 8
 };
 
 enum lwan_request_flags {
-    REQUEST_ALL_FLAGS          = -1,
+    REQUEST_ALL_FLAGS = -1,
 
     /* Shift values to make easier to build flags while booting a
      * request-processing coroutine.
@@ -211,35 +213,35 @@ enum lwan_request_flags {
     REQUEST_ALLOW_PROXY_REQS_SHIFT = 6,
     REQUEST_ALLOW_CORS_SHIFT = 8,
 
-    REQUEST_METHOD_MASK        = 1<<0 | 1<<1 | 1<<2,
-    REQUEST_METHOD_GET         = 1<<0,
-    REQUEST_METHOD_POST        = 1<<1,
-    REQUEST_METHOD_HEAD        = 1<<0 | 1<<1,
-    REQUEST_METHOD_OPTIONS     = 1<<2,
-    REQUEST_METHOD_DELETE      = 1<<2 | 1<<0,
+    REQUEST_METHOD_MASK = 1 << 0 | 1 << 1 | 1 << 2,
+    REQUEST_METHOD_GET = 1 << 0,
+    REQUEST_METHOD_POST = 1 << 1,
+    REQUEST_METHOD_HEAD = 1 << 0 | 1 << 1,
+    REQUEST_METHOD_OPTIONS = 1 << 2,
+    REQUEST_METHOD_DELETE = 1 << 2 | 1 << 0,
 
-    REQUEST_ACCEPT_DEFLATE     = 1<<3,
-    REQUEST_ACCEPT_GZIP        = 1<<4,
-    REQUEST_IS_HTTP_1_0        = 1<<5,
-    REQUEST_ALLOW_PROXY_REQS   = 1<<REQUEST_ALLOW_PROXY_REQS_SHIFT,
-    REQUEST_PROXIED            = 1<<7,
-    REQUEST_ALLOW_CORS         = 1<<REQUEST_ALLOW_CORS_SHIFT,
+    REQUEST_ACCEPT_DEFLATE = 1 << 3,
+    REQUEST_ACCEPT_GZIP = 1 << 4,
+    REQUEST_IS_HTTP_1_0 = 1 << 5,
+    REQUEST_ALLOW_PROXY_REQS = 1 << REQUEST_ALLOW_PROXY_REQS_SHIFT,
+    REQUEST_PROXIED = 1 << 7,
+    REQUEST_ALLOW_CORS = 1 << REQUEST_ALLOW_CORS_SHIFT,
 
-    RESPONSE_SENT_HEADERS      = 1<<9,
-    RESPONSE_CHUNKED_ENCODING  = 1<<10,
-    RESPONSE_NO_CONTENT_LENGTH = 1<<11,
-    RESPONSE_URL_REWRITTEN     = 1<<12,
+    RESPONSE_SENT_HEADERS = 1 << 9,
+    RESPONSE_CHUNKED_ENCODING = 1 << 10,
+    RESPONSE_NO_CONTENT_LENGTH = 1 << 11,
+    RESPONSE_URL_REWRITTEN = 1 << 12,
 };
 
 enum lwan_connection_flags {
-    CONN_MASK               = -1,
-    CONN_KEEP_ALIVE         = 1<<0,
-    CONN_IS_ALIVE           = 1<<1,
-    CONN_SHOULD_RESUME_CORO = 1<<2,
-    CONN_WRITE_EVENTS       = 1<<3,
-    CONN_MUST_READ          = 1<<4,
-    CONN_SUSPENDED_BY_TIMER = 1<<5,
-    CONN_RESUMED_FROM_TIMER = 1<<6,
+    CONN_MASK = -1,
+    CONN_KEEP_ALIVE = 1 << 0,
+    CONN_IS_ALIVE = 1 << 1,
+    CONN_SHOULD_RESUME_CORO = 1 << 2,
+    CONN_WRITE_EVENTS = 1 << 3,
+    CONN_MUST_READ = 1 << 4,
+    CONN_SUSPENDED_BY_TIMER = 1 << 5,
+    CONN_RESUMED_FROM_TIMER = 1 << 6,
 };
 
 enum lwan_connection_coro_yield {
@@ -261,7 +263,8 @@ struct lwan_response {
     const struct lwan_key_value *headers;
 
     struct {
-        enum lwan_http_status (*callback)(struct lwan_request *request, void *data);
+        enum lwan_http_status (*callback)(struct lwan_request *request,
+                                          void *data);
         void *data;
     } stream;
 };
@@ -305,8 +308,8 @@ struct lwan_request {
     struct {
         time_t if_modified_since;
         struct {
-          off_t from;
-          off_t to;
+            off_t from;
+            off_t to;
         } range;
         struct lwan_value *body;
         struct lwan_value *content_type;
@@ -341,7 +344,9 @@ struct lwan_handler_info {
 };
 
 struct lwan_url_map {
-    enum lwan_http_status (*handler)(struct lwan_request *request, struct lwan_response *response, void *data);
+    enum lwan_http_status (*handler)(struct lwan_request *request,
+                                     struct lwan_response *response,
+                                     void *data);
     void *data;
 
     const char *prefix;
@@ -414,23 +419,32 @@ void lwan_set_url_map(struct lwan *l, const struct lwan_url_map *map);
 void lwan_main_loop(struct lwan *l);
 
 void lwan_response(struct lwan_request *request, enum lwan_http_status status);
-void lwan_default_response(struct lwan_request *request, enum lwan_http_status status);
-size_t lwan_prepare_response_header(struct lwan_request *request, enum lwan_http_status status, char header_buffer[], size_t header_buffer_size)
+void lwan_default_response(struct lwan_request *request,
+                           enum lwan_http_status status);
+size_t lwan_prepare_response_header(struct lwan_request *request,
+                                    enum lwan_http_status status,
+                                    char header_buffer[],
+                                    size_t header_buffer_size)
     __attribute__((warn_unused_result));
 
-const char *lwan_request_get_post_param(struct lwan_request *request, const char *key)
+const char *lwan_request_get_post_param(struct lwan_request *request,
+                                        const char *key)
     __attribute__((warn_unused_result, pure));
-const char *lwan_request_get_query_param(struct lwan_request *request, const char *key)
+const char *lwan_request_get_query_param(struct lwan_request *request,
+                                         const char *key)
     __attribute__((warn_unused_result, pure));
-const char * lwan_request_get_cookie(struct lwan_request *request, const char *key)
+const char *lwan_request_get_cookie(struct lwan_request *request,
+                                    const char *key)
     __attribute__((warn_unused_result, pure));
 
 void lwan_request_sleep(struct lwan_request *request, uint64_t ms);
 
-bool lwan_response_set_chunked(struct lwan_request *request, enum lwan_http_status status);
+bool lwan_response_set_chunked(struct lwan_request *request,
+                               enum lwan_http_status status);
 void lwan_response_send_chunk(struct lwan_request *request);
 
-bool lwan_response_set_event_stream(struct lwan_request *request, enum lwan_http_status status);
+bool lwan_response_set_event_stream(struct lwan_request *request,
+                                    enum lwan_http_status status);
 void lwan_response_send_event(struct lwan_request *request, const char *event);
 
 const char *lwan_http_status_as_string(enum lwan_http_status status)
@@ -450,15 +464,19 @@ void lwan_straitjacket_enforce(const struct lwan_straitjacket *sj);
 
 const struct lwan_config *lwan_get_default_config(void);
 
-int lwan_connection_get_fd(const struct lwan *lwan, const struct lwan_connection *conn)
+int lwan_connection_get_fd(const struct lwan *lwan,
+                           const struct lwan_connection *conn)
     __attribute__((pure)) __attribute__((warn_unused_result));
 
-const char *lwan_request_get_remote_address(struct lwan_request *request,
-            char buffer[ENFORCE_STATIC_BUFFER_LENGTH INET6_ADDRSTRLEN])
+const char *lwan_request_get_remote_address(
+    struct lwan_request *request,
+    char buffer[ENFORCE_STATIC_BUFFER_LENGTH INET6_ADDRSTRLEN])
     __attribute__((warn_unused_result));
 
-int lwan_format_rfc_time(const time_t in, char out[ENFORCE_STATIC_BUFFER_LENGTH 30]);
-int lwan_parse_rfc_time(const char in[ENFORCE_STATIC_BUFFER_LENGTH 30], time_t *out);
+int lwan_format_rfc_time(const time_t in,
+                         char out[ENFORCE_STATIC_BUFFER_LENGTH 30]);
+int lwan_parse_rfc_time(const char in[ENFORCE_STATIC_BUFFER_LENGTH 30],
+                        time_t *out);
 
 static inline enum lwan_request_flags
 lwan_request_get_method(const struct lwan_request *request)
@@ -466,6 +484,6 @@ lwan_request_get_method(const struct lwan_request *request)
     return (enum lwan_request_flags)(request->flags & REQUEST_METHOD_MASK);
 }
 
-#if defined (__cplusplus)
+#if defined(__cplusplus)
 }
 #endif
