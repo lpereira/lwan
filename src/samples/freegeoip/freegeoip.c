96f4751312672dde8229738749fffebd4db99686
Leandro Pereira <leandro@hardinfo.org>
2018-10-19 07:59:52 -0700

    Reindent freegeoip.c

--- src/samples/freegeoip/freegeoip.c
+++ src/samples/freegeoip/freegeoip.c
@@ -17,14 +17,15 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  */
 
 #define _DEFAULT_SOURCE
 #include <arpa/inet.h>
-#include <string.h>
-#include <stdlib.h>
 #include <sqlite3.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "lwan.h"
 #include "lwan-cache.h"
@@ -70,74 +71,71 @@ static const struct lwan_var_descriptor template_descriptor[] = {
     TPL_VAR_STR(struct ip_info, metro.area),
     TPL_VAR_STR(struct ip_info, ip),
     TPL_VAR_STR(struct ip_info, callback),
-    TPL_VAR_SENTINEL
+    TPL_VAR_SENTINEL,
 };
 
-static const char json_template_str[] = \
-    "{{callback?}}{{callback}}({{/callback?}}" \
-    "{" \
-    "\"country_code\":\"{{country.code}}\"," \
-    "\"country_name\":\"{{country.name}}\"," \
-    "\"region_code\":\"{{region.code}}\"," \
-    "\"region_name\":\"{{region.name}}\"," \
-    "\"city\":\"{{city.name}}\"," \
-    "\"zipcode\":\"{{city.zip_code}}\"," \
-    "\"latitude\":{{latitude}}," \
-    "\"longitude\":{{longitude}}," \
-    "\"metro_code\":\"{{metro.code}}\"," \
-    "\"areacode\":\"{{metro.area}}\"," \
-    "\"ip\":\"{{ip}}\"" \
+static const char json_template_str[] =
+    "{{callback?}}{{callback}}({{/callback?}}"
+    "{"
+    "\"country_code\":\"{{country.code}}\","
+    "\"country_name\":\"{{country.name}}\","
+    "\"region_code\":\"{{region.code}}\","
+    "\"region_name\":\"{{region.name}}\","
+    "\"city\":\"{{city.name}}\","
+    "\"zipcode\":\"{{city.zip_code}}\","
+    "\"latitude\":{{latitude}},"
+    "\"longitude\":{{longitude}},"
+    "\"metro_code\":\"{{metro.code}}\","
+    "\"areacode\":\"{{metro.area}}\","
+    "\"ip\":\"{{ip}}\""
     "}"
     "{{callback?}});{{/callback?}}";
 
-static const char xml_template_str[] = \
-    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" \
-    "<Response>" \
-    "<Ip>{{ip}}</Ip>" \
-    "<CountryCode>{{country.code}}</CountryCode>" \
-    "<CountryName>{{country.name}}</CountryName>" \
-    "<RegionCode>{{region.code}}</RegionCode>" \
-    "<RegionName>{{region.name}}</RegionName>" \
-    "<City>{{city.name}}</City>" \
-    "<ZipCode>{{city.zip_code}}</ZipCode>" \
-    "<Latitude>{{latitude}}</Latitude>" \
-    "<Longitude>{{longitude}}</Longitude>" \
-    "<MetroCode>{{metro.code}}</MetroCode>" \
-    "<AreaCode>{{metro.area}}</AreaCode>" \
+static const char xml_template_str[] =
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+    "<Response>"
+    "<Ip>{{ip}}</Ip>"
+    "<CountryCode>{{country.code}}</CountryCode>"
+    "<CountryName>{{country.name}}</CountryName>"
+    "<RegionCode>{{region.code}}</RegionCode>"
+    "<RegionName>{{region.name}}</RegionName>"
+    "<City>{{city.name}}</City>"
+    "<ZipCode>{{city.zip_code}}</ZipCode>"
+    "<Latitude>{{latitude}}</Latitude>"
+    "<Longitude>{{longitude}}</Longitude>"
+    "<MetroCode>{{metro.code}}</MetroCode>"
+    "<AreaCode>{{metro.area}}</AreaCode>"
     "</Response>";
 
-static const char csv_template_str[] = \
-    "\"{{ip}}\"," \
-    "\"{{country.code}}\"," \
-    "\"{{country.name}}\"," \
-    "\"{{region.code}}\"," \
-    "\"{{region.name}}\"," \
-    "\"{{city.name}}\","
-    "\"{{city.zip_code}}\"," \
-    "\"{{latitude}}\"," \
-    "\"{{longitude}}\"," \
-    "\"{{metro.code}}\"," \
-    "\"{{metro.area}}\"";
+static const char csv_template_str[] = "\"{{ip}}\","
+                                       "\"{{country.code}}\","
+                                       "\"{{country.name}}\","
+                                       "\"{{region.code}}\","
+                                       "\"{{region.name}}\","
+                                       "\"{{city.name}}\","
+                                       "\"{{city.zip_code}}\","
+                                       "\"{{latitude}}\","
+                                       "\"{{longitude}}\","
+                                       "\"{{metro.code}}\","
+                                       "\"{{metro.area}}\"";
 
-
-static const char ip_to_city_query[] = \
-    "SELECT " \
-    "   city_location.country_code, country_blocks.country_name," \
-    "   city_location.region_code, region_names.region_name," \
-    "   city_location.city_name, city_location.postal_code," \
-    "   city_location.latitude, city_location.longitude," \
-    "   city_location.metro_code, city_location.area_code " \
-    "FROM city_blocks " \
-    "   NATURAL JOIN city_location " \
-    "   INNER JOIN country_blocks ON " \
-    "      city_location.country_code = country_blocks.country_code " \
-    "   INNER JOIN region_names ON " \
-    "      city_location.country_code = region_names.country_code " \
-    "      AND " \
-    "      city_location.region_code = region_names.region_code " \
-    "WHERE city_blocks.ip_start <= ? " \
+static const char ip_to_city_query[] =
+    "SELECT "
+    "   city_location.country_code, country_blocks.country_name,"
+    "   city_location.region_code, region_names.region_name,"
+    "   city_location.city_name, city_location.postal_code,"
+    "   city_location.latitude, city_location.longitude,"
+    "   city_location.metro_code, city_location.area_code "
+    "FROM city_blocks "
+    "   NATURAL JOIN city_location "
+    "   INNER JOIN country_blocks ON "
+    "      city_location.country_code = country_blocks.country_code "
+    "   INNER JOIN region_names ON "
+    "      city_location.country_code = region_names.country_code "
+    "      AND "
+    "      city_location.region_code = region_names.region_code "
+    "WHERE city_blocks.ip_start <= ? "
     "ORDER BY city_blocks.ip_start DESC LIMIT 1";
-
 
 union ip_to_octet {
     unsigned char octet[sizeof(in_addr_t)];
@@ -150,8 +148,10 @@ struct ip_net {
 };
 
 /* http://en.wikipedia.org/wiki/Reserved_IP_addresses */
-#define ADDRESS(o1, o2, o3, o4) \
-    { .octet[0] = o1, .octet[1] = o2, .octet[2] = o3, .octet[3] = o4 }
+#define ADDRESS(o1, o2, o3, o4)                                                \
+    {                                                                          \
+        .octet[0] = o1, .octet[1] = o2, .octet[2] = o3, .octet[3] = o4         \
+    }
 
 static const struct ip_net reserved_ips[] = {
     {ADDRESS(0, 0, 0, 0), ADDRESS(255, 0, 0, 0)},
@@ -174,7 +174,6 @@ static const struct ip_net reserved_ips[] = {
 
 #undef ADDRESS
 
-
 #if QUERIES_PER_HOUR != 0
 struct query_limit {
     struct cache_entry base;
@@ -187,18 +186,20 @@ static struct cache *query_limit;
 static struct cache *cache = NULL;
 static sqlite3 *db = NULL;
 
-static bool
-net_contains_ip(const struct ip_net *net, in_addr_t ip)
+static bool net_contains_ip(const struct ip_net *net, in_addr_t ip)
 {
-    union ip_to_octet _ip = { .ip = ip };
-    return (net->ip.octet[0] & net->mask.octet[0]) == (_ip.octet[0] & net->mask.octet[0]) && \
-        (net->ip.octet[1] & net->mask.octet[1]) == (_ip.octet[1] & net->mask.octet[1]) && \
-        (net->ip.octet[2] & net->mask.octet[2]) == (_ip.octet[2] & net->mask.octet[2]) && \
-        (net->ip.octet[3] & net->mask.octet[3]) == (_ip.octet[3] & net->mask.octet[3]);
+    union ip_to_octet _ip = {.ip = ip};
+    return (net->ip.octet[0] & net->mask.octet[0]) ==
+               (_ip.octet[0] & net->mask.octet[0]) &&
+           (net->ip.octet[1] & net->mask.octet[1]) ==
+               (_ip.octet[1] & net->mask.octet[1]) &&
+           (net->ip.octet[2] & net->mask.octet[2]) ==
+               (_ip.octet[2] & net->mask.octet[2]) &&
+           (net->ip.octet[3] & net->mask.octet[3]) ==
+               (_ip.octet[3] & net->mask.octet[3]);
 }
 
-static bool
-is_reserved_ip(in_addr_t ip)
+static bool is_reserved_ip(in_addr_t ip)
 {
     size_t i;
     for (i = 0; i < N_ELEMENTS(reserved_ips); i++) {
@@ -208,9 +209,8 @@ is_reserved_ip(in_addr_t ip)
     return false;
 }
 
-static void
-destroy_ipinfo(struct cache_entry *entry,
-            void *context __attribute__((unused)))
+static void destroy_ipinfo(struct cache_entry *entry,
+                           void *context __attribute__((unused)))
 {
     struct ip_info *ip_info = (struct ip_info *)entry;
 
@@ -229,8 +229,7 @@ destroy_ipinfo(struct cache_entry *entry,
     free(ip_info);
 }
 
-static ALWAYS_INLINE char *
-text_column_helper(sqlite3_stmt *stmt, int ind)
+static ALWAYS_INLINE char *text_column_helper(sqlite3_stmt *stmt, int ind)
 {
     const unsigned char *value;
 
@@ -238,8 +237,8 @@ text_column_helper(sqlite3_stmt *stmt, int ind)
     return value ? strdup((char *)value) : NULL;
 }
 
-static struct cache_entry *
-create_ipinfo(const char *key, void *context __attribute__((unused)))
+static struct cache_entry *create_ipinfo(const char *key,
+                                         void *context __attribute__((unused)))
 {
     sqlite3_stmt *stmt;
     struct ip_info *ip_info = NULL;
@@ -258,8 +257,7 @@ create_ipinfo(const char *key, void *context __attribute__((unused)))
         goto end_no_finalize;
     }
 
-    if (sqlite3_prepare(db, ip_to_city_query,
-                        sizeof(ip_to_city_query) - 1,
+    if (sqlite3_prepare(db, ip_to_city_query, sizeof(ip_to_city_query) - 1,
                         &stmt, NULL) != SQLITE_OK)
         goto end_no_finalize;
 
@@ -297,9 +295,10 @@ end_no_finalize:
 }
 
 #if QUERIES_PER_HOUR != 0
-static struct cache_entry *
-create_query_limit(const char *key __attribute__((unused)),
-            void *context __attribute__((unused)))
+static struct cache_entry *create_query_limit(const char *key
+                                              __attribute__((unused)),
+                                              void *context
+                                              __attribute__((unused)))
 {
     struct query_limit *entry = malloc(sizeof(*entry));
     if (LIKELY(entry))
@@ -307,16 +306,15 @@ create_query_limit(const char *key __attribute__((unused)),
     return (struct cache_entry *)entry;
 }
 
-static void
-destroy_query_limit(struct cache_entry *entry,
-            void *context __attribute__((unused)))
+static void destroy_query_limit(struct cache_entry *entry,
+                                void *context __attribute__((unused)))
 {
     free(entry);
 }
 #endif
 
-static struct ip_info *
-internal_query(struct lwan_request *request, const char *ip_address)
+static struct ip_info *internal_query(struct lwan_request *request,
+                                      const char *ip_address)
 {
     const char *query;
 
@@ -329,8 +327,8 @@ internal_query(struct lwan_request *request, const char *ip_address)
     if (UNLIKELY(!query))
         return NULL;
 
-    return (struct ip_info *)cache_coro_get_and_ref_entry(cache,
-                request->conn->coro, query);
+    return (struct ip_info *)cache_coro_get_and_ref_entry(
+        cache, request->conn->coro, query);
 }
 
 #if QUERIES_PER_HOUR != 0
@@ -340,8 +338,8 @@ static bool is_rate_limited(const char *ip_address)
     int error;
     struct query_limit *limit;
 
-    limit = (struct query_limit *)
-                cache_get_and_ref_entry(query_limit, ip_address, &error);
+    limit = (struct query_limit *)cache_get_and_ref_entry(query_limit,
+                                                          ip_address, &error);
     if (!limit)
         return true;
 
@@ -400,48 +398,46 @@ static struct template_mime compile_template(const char *template,
     return (struct template_mime){.tpl = tpl, .mime_type = mime_type};
 }
 
-int
-main(void)
+int main(void)
 {
     struct lwan l;
 
     lwan_init(&l);
 
-    struct template_mime json_tpl = compile_template(json_template_str,
-        "application/json; charset=UTF-8");
-    struct template_mime csv_tpl = compile_template(csv_template_str,
-        "application/csv; charset=UTF-8");
-    struct template_mime xml_tpl = compile_template(xml_template_str,
-        "text/plain; charset=UTF-8");
+    struct template_mime json_tpl =
+        compile_template(json_template_str, "application/json; charset=UTF-8");
+    struct template_mime csv_tpl =
+        compile_template(csv_template_str, "application/csv; charset=UTF-8");
+    struct template_mime xml_tpl =
+        compile_template(xml_template_str, "text/plain; charset=UTF-8");
 
-    int result = sqlite3_open_v2("./db/ipdb.sqlite", &db,
-                                 SQLITE_OPEN_READONLY, NULL);
+    int result =
+        sqlite3_open_v2("./db/ipdb.sqlite", &db, SQLITE_OPEN_READONLY, NULL);
     if (result != SQLITE_OK)
-        lwan_status_critical("Could not open database: %s",
-                    sqlite3_errmsg(db));
+        lwan_status_critical("Could not open database: %s", sqlite3_errmsg(db));
     cache = cache_create(create_ipinfo, destroy_ipinfo, NULL, 10);
 
 #if QUERIES_PER_HOUR != 0
     lwan_status_info("Limiting to %d queries per hour per client",
-                QUERIES_PER_HOUR);
-    query_limit = cache_create(create_query_limit,
-                destroy_query_limit, NULL, 3600);
+                     QUERIES_PER_HOUR);
+    query_limit =
+        cache_create(create_query_limit, destroy_query_limit, NULL, 3600);
 #else
     lwan_status_info("Rate-limiting disabled");
 #endif
 
     const struct lwan_url_map default_map[] = {
-        { .prefix = "/json/",
-          .handler = LWAN_HANDLER_REF(templated_output),
-          .data = &json_tpl },
-        { .prefix = "/xml/",
-          .handler = LWAN_HANDLER_REF(templated_output),
-          .data = &xml_tpl },
-        { .prefix = "/csv/",
-          .handler = LWAN_HANDLER_REF(templated_output),
-          .data = &csv_tpl },
-        { .prefix = "/", SERVE_FILES("./static") },
-        { .prefix = NULL }
+        {.prefix = "/json/",
+         .handler = LWAN_HANDLER_REF(templated_output),
+         .data = &json_tpl},
+        {.prefix = "/xml/",
+         .handler = LWAN_HANDLER_REF(templated_output),
+         .data = &xml_tpl},
+        {.prefix = "/csv/",
+         .handler = LWAN_HANDLER_REF(templated_output),
+         .data = &csv_tpl},
+        {.prefix = "/", SERVE_FILES("./static")},
+        {.prefix = NULL},
     };
 
     lwan_set_url_map(&l, default_map);
