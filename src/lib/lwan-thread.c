8dc03d2fe104bd7dec2493ca7afc76eaba4cf83d
Leandro Pereira <leandro@hardinfo.org>
2018-11-07 07:39:32 -0800

    Ensure removed node from death queue points to nowhere
    Not doing this causes an infinite loop while shutting down the DQ,
    as death_queue_empty() returns false.

--- src/lib/lwan-thread.c
+++ src/lib/lwan-thread.c
@@ -78,6 +78,8 @@ static void death_queue_remove(struct death_queue *dq,
     struct lwan_connection *next = death_queue_idx_to_node(dq, node->next);
     next->prev = node->prev;
     prev->next = node->next;
+
+    node->next = node->prev = -1;
 }
 
 static bool death_queue_empty(struct death_queue *dq)
