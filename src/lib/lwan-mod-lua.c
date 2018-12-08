97ae0cfa0d04ffa35ff0cecd4ddd12963c95dc13
Leandro Pereira <leandro@hardinfo.org>
2018-12-08 06:20:38 -0800

    Simplify get_handle_prefix() by using a lookup table

--- src/lib/lwan-mod-lua.c
+++ src/lib/lwan-mod-lua.c
@@ -99,39 +99,30 @@ static void unref_thread(void *data1, void *data2)
     luaL_unref(L, LUA_REGISTRYINDEX, thread_ref);
 }
 
-static ALWAYS_INLINE const char *get_handle_prefix(struct lwan_request *request,
-                                                   size_t *len)
+static ALWAYS_INLINE struct lwan_value
+get_handle_prefix(struct lwan_request *request)
 {
-    switch (lwan_request_get_method(request)) {
-    case REQUEST_METHOD_GET:
-        *len = sizeof("handle_get_");
-        return "handle_get_";
-    case REQUEST_METHOD_POST:
-        *len = sizeof("handle_post_");
-        return "handle_post_";
-    case REQUEST_METHOD_HEAD:
-        *len = sizeof("handle_head_");
-        return "handle_head_";
-    case REQUEST_METHOD_OPTIONS:
-        *len = sizeof("handle_options_");
-        return "handle_options_";
-    case REQUEST_METHOD_DELETE:
-        *len = sizeof("handle_delete_");
-        return "handle_delete_";
-    default:
-        return NULL;
-    }
+    static const struct lwan_value method2name[REQUEST_METHOD_MASK] = {
+#define M(s) {.value = s, .len = sizeof(s) - 1}
+        [REQUEST_METHOD_GET] = M("handle_get_"),
+        [REQUEST_METHOD_POST] = M("handle_post_"),
+        [REQUEST_METHOD_HEAD] = M("handle_head_"),
+        [REQUEST_METHOD_OPTIONS] = M("handle_options_"),
+        [REQUEST_METHOD_DELETE] = M("handle_delete_"),
+#undef M
+    };
+
+    return method2name[lwan_request_get_method(request)];
 }
 
 static bool get_handler_function(lua_State *L, struct lwan_request *request)
 {
     char handler_name[128];
-    size_t handle_prefix_len;
-    const char *handle_prefix = get_handle_prefix(request, &handle_prefix_len);
+    struct lwan_value handle_prefix = get_handle_prefix(request);
 
-    if (UNLIKELY(!handle_prefix))
+    if (UNLIKELY(!handle_prefix.len))
         return false;
-    if (UNLIKELY(request->url.len >= sizeof(handler_name) - handle_prefix_len))
+    if (UNLIKELY(request->url.len >= sizeof(handler_name) - handle_prefix.len))
         return false;
 
     char *url;
@@ -152,10 +143,10 @@ static bool get_handler_function(lua_State *L, struct lwan_request *request)
         url_len = 4;
     }
 
-    if (UNLIKELY((handle_prefix_len + url_len + 1) > sizeof(handler_name)))
+    if (UNLIKELY((handle_prefix.len + url_len + 1) > sizeof(handler_name)))
         return false;
 
-    char *method_name = mempcpy(handler_name, handle_prefix, handle_prefix_len);
+    char *method_name = mempcpy(handler_name, handle_prefix.value, handle_prefix.len);
     memcpy(method_name - 1, url, url_len + 1);
 
     lua_getglobal(L, handler_name);
