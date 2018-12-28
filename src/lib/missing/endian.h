c3c97e7f3c02e078e1ef507b5d1f14ff358b3141
Tomohito Nakayama <tomohito.nakayama@shorthair.local>
2018-12-28 23:54:56 +0900

    Recover text stating public domain license and add more text around history.

--- src/lib/missing/endian.h
+++ src/lib/missing/endian.h
@@ -1,3 +1,13 @@
+// This file originates to a work of Mathias Panzenböck, portable_endian.h, of which he puts in public domain. 
+//
+// License text was as next.
+//
+// "License": Public Domain
+// I, Mathias Panzenböck, place this file hereby into the public domain. Use it at your own risk for whatever you like.
+// In case there are jurisdictions that don't support putting things in the public domain you can also consider it to
+// be "dual licensed" under the BSD, MIT and Apache licenses, if you want to. This code is trivial anyway. Consider it
+// an example on how to get the endian conversion functions on different platforms.
+
 #ifndef PORTABLE_ENDIAN_H__
 #define PORTABLE_ENDIAN_H__
 
