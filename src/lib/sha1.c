ea249cb78ccd413c20bd9d0cebc9e924e6c3e142
Leandro Pereira <leandro@hardinfo.org>
2019-01-21 12:18:23 -0800

    Use <endian.h> in sha1.c to clean it up even further

--- src/lib/sha1.c
+++ src/lib/sha1.c
@@ -12,7 +12,6 @@ A million repetitions of "a"
   34AA973C D4C4DAA4 F61EEB2B DBAD2731 6534016F
 */
 
-/* #define LITTLE_ENDIAN * This should be #define'd already, if true. */
 /* #define SHA1HANDSOFF * Copies data before messing with it. */
 
 #define SHA1HANDSOFF
@@ -21,70 +20,13 @@ A million repetitions of "a"
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h> /* for u_int*_t */
-
-#ifndef BYTE_ORDER
-#if (BSD >= 199103)
-#include <machine/endian.h>
-#else
-#if defined(linux) || defined(__linux__)
 #include <endian.h>
-#else
-#define LITTLE_ENDIAN 1234 /* least-significant byte first (vax, pc) */
-#define BIG_ENDIAN 4321    /* most-significant byte first (IBM, net) */
-#define PDP_ENDIAN 3412    /* LSB first in word, MSW first in long (pdp)*/
-
-#if defined(vax) || defined(ns32000) || defined(sun386) ||                     \
-    defined(__i386__) || defined(MIPSEL) || defined(_MIPSEL) ||                \
-    defined(BIT_ZERO_ON_RIGHT) || defined(__alpha__) || defined(__alpha)
-#define BYTE_ORDER LITTLE_ENDIAN
-#endif
-
-#if defined(sel) || defined(pyr) || defined(mc68000) || defined(sparc) ||      \
-    defined(is68k) || defined(tahoe) || defined(ibm032) || defined(ibm370) ||  \
-    defined(MIPSEB) || defined(_MIPSEB) || defined(_IBMR2) || defined(DGUX) || \
-    defined(apollo) || defined(__convex__) || defined(_CRAY) ||                \
-    defined(__hppa) || defined(__hp9000) || defined(__hp9000s300) ||           \
-    defined(__hp9000s700) || defined(BIT_ZERO_ON_LEFT) || defined(m68k) ||     \
-    defined(__sparc)
-#define BYTE_ORDER BIG_ENDIAN
-#endif
-#endif /* linux */
-#endif /* BSD */
-#endif /* BYTE_ORDER */
-
-#if defined(__BYTE_ORDER) && !defined(BYTE_ORDER)
-#if (__BYTE_ORDER == __LITTLE_ENDIAN)
-#define BYTE_ORDER LITTLE_ENDIAN
-#else
-#define BYTE_ORDER BIG_ENDIAN
-#endif
-#endif
-
-#if !defined(BYTE_ORDER) ||                                                    \
-    (BYTE_ORDER != BIG_ENDIAN && BYTE_ORDER != LITTLE_ENDIAN &&                \
-     BYTE_ORDER != PDP_ENDIAN)
-/* you must determine what the correct bit order is for
- * your compiler - the next line is an intentional error
- * which will force your compiles to bomb until you fix
- * the above macros.
- */
-#error "Undefined or invalid BYTE_ORDER"
-#endif
 
 #define rol(value, bits) (((value) << (bits)) | ((value) >> (32 - (bits))))
 
 /* blk0() and blk() perform the initial expand. */
 /* I got the idea of expanding during the round function from SSLeay */
-#if BYTE_ORDER == LITTLE_ENDIAN
-#define blk0(i)                                                                \
-    (block->l[i] = (rol(block->l[i], 24) & 0xFF00FF00) |                       \
-                   (rol(block->l[i], 8) & 0x00FF00FF))
-#elif BYTE_ORDER == BIG_ENDIAN
-#define blk0(i) block->l[i]
-#else
-#error "Endianness not defined!"
-#endif
+#define blk0(i) (block->l[i] = htobe32(block->l[i]))
 #define blk(i)                                                                 \
     (block->l[i & 15] = rol(block->l[(i + 13) & 15] ^ block->l[(i + 8) & 15] ^ \
                                 block->l[(i + 2) & 15] ^ block->l[i & 15],     \
