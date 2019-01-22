7d217acbda027e8a1cd6c20feea57e9c72feb1c4
Leandro Pereira <leandro@hardinfo.org>
2019-01-21 16:02:06 -0800

    Re-roll loop in sha1_transform()
    This generates smaller code.

--- src/lib/sha1.c
+++ src/lib/sha1.c
@@ -22,11 +22,11 @@ A million repetitions of "a"
 
 /* blk0() and blk() perform the initial expand. */
 /* I got the idea of expanding during the round function from SSLeay */
-#define blk0(i) (block->l[i] = htobe32(block->l[i]))
+#define blk0(i) (block[i] = htobe32(block[i]))
 #define blk(i)                                                                 \
-    (block->l[i & 15] = rol(block->l[(i + 13) & 15] ^ block->l[(i + 8) & 15] ^ \
-                                block->l[(i + 2) & 15] ^ block->l[i & 15],     \
-                            1))
+    (block[i & 15] = rol(block[(i + 13) & 15] ^ block[(i + 8) & 15] ^          \
+                             block[(i + 2) & 15] ^ block[i & 15],              \
+                         1))
 
 /* (R0+R1), R2, R3, R4 are the different operations used in SHA1 */
 #define R0(v, w, x, y, z, i)                                                   \
@@ -47,121 +47,48 @@ A million repetitions of "a"
 
 /* Hash a single 512-bit block. This is the core of the algorithm. */
 
-static void sha1_transform(uint32_t state[5], const unsigned char buffer[64])
-{
-    uint32_t a, b, c, d, e;
-    typedef union {
-        unsigned char c[64];
-        uint32_t l[16];
-    } CHAR64LONG16;
-    CHAR64LONG16 block[1]; /* use array to appear as a pointer */
+#define STATE(i)		state[(index + (i)) % 5]
 
-    memcpy(block, buffer, 64);
+static void sha1_transform(uint32_t orig_state[5], const unsigned char buffer[64])
+{
+    uint32_t state[5];
+    uint32_t block[16];
+    int round = 0;
+    int index = 0;
+
+    memcpy(block, buffer, sizeof(block));
 
     /* Copy context->state[] to working vars */
-    a = state[0];
-    b = state[1];
-    c = state[2];
-    d = state[3];
-    e = state[4];
-    /* 4 rounds of 20 operations each. Loop unrolled. */
-    R0(a, b, c, d, e, 0);
-    R0(e, a, b, c, d, 1);
-    R0(d, e, a, b, c, 2);
-    R0(c, d, e, a, b, 3);
-    R0(b, c, d, e, a, 4);
-    R0(a, b, c, d, e, 5);
-    R0(e, a, b, c, d, 6);
-    R0(d, e, a, b, c, 7);
-    R0(c, d, e, a, b, 8);
-    R0(b, c, d, e, a, 9);
-    R0(a, b, c, d, e, 10);
-    R0(e, a, b, c, d, 11);
-    R0(d, e, a, b, c, 12);
-    R0(c, d, e, a, b, 13);
-    R0(b, c, d, e, a, 14);
-    R0(a, b, c, d, e, 15);
-    R1(e, a, b, c, d, 16);
-    R1(d, e, a, b, c, 17);
-    R1(c, d, e, a, b, 18);
-    R1(b, c, d, e, a, 19);
+    memcpy(state, orig_state, sizeof(state));
 
-    R2(a, b, c, d, e, 20);
-    R2(e, a, b, c, d, 21);
-    R2(d, e, a, b, c, 22);
-    R2(c, d, e, a, b, 23);
-    R2(b, c, d, e, a, 24);
-    R2(a, b, c, d, e, 25);
-    R2(e, a, b, c, d, 26);
-    R2(d, e, a, b, c, 27);
-    R2(c, d, e, a, b, 28);
-    R2(b, c, d, e, a, 29);
-    R2(a, b, c, d, e, 30);
-    R2(e, a, b, c, d, 31);
-    R2(d, e, a, b, c, 32);
-    R2(c, d, e, a, b, 33);
-    R2(b, c, d, e, a, 34);
-    R2(a, b, c, d, e, 35);
-    R2(e, a, b, c, d, 36);
-    R2(d, e, a, b, c, 37);
-    R2(c, d, e, a, b, 38);
-    R2(b, c, d, e, a, 39);
-    R3(a, b, c, d, e, 40);
-    R3(e, a, b, c, d, 41);
-    R3(d, e, a, b, c, 42);
-    R3(c, d, e, a, b, 43);
-    R3(b, c, d, e, a, 44);
-    R3(a, b, c, d, e, 45);
-    R3(e, a, b, c, d, 46);
-    R3(d, e, a, b, c, 47);
-    R3(c, d, e, a, b, 48);
-    R3(b, c, d, e, a, 49);
-    R3(a, b, c, d, e, 50);
-    R3(e, a, b, c, d, 51);
-    R3(d, e, a, b, c, 52);
-    R3(c, d, e, a, b, 53);
-    R3(b, c, d, e, a, 54);
-    R3(a, b, c, d, e, 55);
-    R3(e, a, b, c, d, 56);
-    R3(d, e, a, b, c, 57);
-    R3(c, d, e, a, b, 58);
-    R3(b, c, d, e, a, 59);
-    R4(a, b, c, d, e, 60);
-    R4(e, a, b, c, d, 61);
-    R4(d, e, a, b, c, 62);
-    R4(c, d, e, a, b, 63);
-    R4(b, c, d, e, a, 64);
-    R4(a, b, c, d, e, 65);
-    R4(e, a, b, c, d, 66);
-    R4(d, e, a, b, c, 67);
-    R4(c, d, e, a, b, 68);
-    R4(b, c, d, e, a, 69);
-    R4(a, b, c, d, e, 70);
-    R4(e, a, b, c, d, 71);
-    R4(d, e, a, b, c, 72);
-    R4(c, d, e, a, b, 73);
-    R4(b, c, d, e, a, 74);
-    R4(a, b, c, d, e, 75);
-    R4(e, a, b, c, d, 76);
-    R4(d, e, a, b, c, 77);
-    R4(c, d, e, a, b, 78);
-    R4(b, c, d, e, a, 79);
+    /* 4 rounds of 20 operations each. */
+    for (; round < 16; round++, index += 4) {
+        R0(STATE(0), STATE(1), STATE(2), STATE(3), STATE(4), round);
+    }
+    for (; round < 20; round++, index += 4) {
+        R1(STATE(0), STATE(1), STATE(2), STATE(3), STATE(4), round);
+    }
+
+    for (; round < 40; round++, index += 4) {
+        R2(STATE(0), STATE(1), STATE(2), STATE(3), STATE(4), round);
+    }
+
+    for (; round < 60; round++, index += 4) {
+        R3(STATE(0), STATE(1), STATE(2), STATE(3), STATE(4), round);
+    }
+
+    for (; round < 80; round++, index += 4) {
+        R4(STATE(0), STATE(1), STATE(2), STATE(3), STATE(4), round);
+    }
+
     /* Add the working vars back into context.state[] */
-    state[0] += a;
-    state[1] += b;
-    state[2] += c;
-    state[3] += d;
-    state[4] += e;
+    for (int i = 0; i < 5; i++)
+        orig_state[i] += state[i];
 
-    /* Wipe variables */
-    a = b = c = d = e = 0;
-    __asm__ volatile(""
-                     :
-                     : "g"(&a), "g"(&b), "g"(&c), "g"(&d), "g"(&e)
-                     : "memory");
-
-    memset(block, '\0', sizeof(block));
-    __asm__ volatile("" : : "g"(block) : "memory");
+    /* Wipe transient state */
+    memset(state, 0, sizeof(state));
+    memset(block, 0, sizeof(block));
+    __asm__ volatile("" : : "g"(block), "g"(state) : "memory");
 }
 
 /* sha1_init - Initialize new context */
