5e1ddd468402824e3068b367f870fa96c9dacc88
Leandro Pereira <leandro@hardinfo.org>
2019-02-17 15:24:13 -0800

    Use X macros for HTTP status tables, too
    Just one place to add new HTTP status; no need to change anything in
    lwan-table.c now.

--- src/lib/lwan.h
+++ src/lib/lwan.h
@@ -172,37 +172,39 @@ static ALWAYS_INLINE int16_t string_as_int16(const char *s)
 #define ENFORCE_STATIC_BUFFER_LENGTH static
 #endif
 
+#define FOR_EACH_HTTP_STATUS(X)                                                                                                             \
+    X(SWITCHING_PROTOCOLS, 101, "Switching protocols", "Protocol is switching over from HTTP")                                              \
+    X(OK, 200, "OK", "Success")                                                                                                             \
+    X(PARTIAL_CONTENT, 206, "Partial content", "Delivering part of requested resource")                                                     \
+    X(MOVED_PERMANENTLY, 301, "Moved permanently", "This content has moved to another place")                                               \
+    X(NOT_MODIFIED, 304, "Not modified", "The content has not changed since previous request")                                              \
+    X(TEMPORARY_REDIRECT, 307, "Temporary Redirect", "This content can be temporarily found at a different location")                       \
+    X(BAD_REQUEST, 400, "Bad request", "The client has issued a bad request")                                                               \
+    X(NOT_AUTHORIZED, 401, "Not authorized", "Client has no authorization to access this resource")                                         \
+    X(FORBIDDEN, 403, "Forbidden", "Access to this resource has been denied")                                                               \
+    X(NOT_FOUND, 404, "Not found", "The requested resource could not be found on this server")                                              \
+    X(NOT_ALLOWED, 405, "Not allowed", "The requested method is not allowed by this server")                                                \
+    X(TIMEOUT, 408, "Request timeout", "Client did not produce a request within expected timeframe")                                        \
+    X(TOO_LARGE, 413, "Request too large", "The request entity is too large")                                                               \
+    X(RANGE_UNSATISFIABLE, 416, "Requested range unsatisfiable", "The server can't supply the requested portion of the requested resource") \
+    X(I_AM_A_TEAPOT, 418, "I'm a teapot", "Client requested to brew coffee but device is a teapot")                                         \
+    X(CLIENT_TOO_HIGH, 420, "Client too high", "Client is too high to make a request")                                                      \
+    X(INTERNAL_ERROR, 500, "Internal server error", "The server encountered an internal error that couldn't be recovered from")             \
+    X(NOT_IMPLEMENTED, 501, "Not implemented", "Server lacks the ability to fulfil the request")                                            \
+    X(UNAVAILABLE, 503, "Service unavailable", "The server is either overloaded or down for maintenance")                                   \
+    X(SERVER_TOO_HIGH, 520, "Server too high", "The server is too high to answer the request")
+
+#define GENERATE_ENUM_ITEM(id, code, short, long) HTTP_ ## id = code,
 enum lwan_http_status {
     HTTP_CLASS__INFORMATIONAL = 100,
-    HTTP_SWITCHING_PROTOCOLS = 101,
-
     HTTP_CLASS__SUCCESS = 200,
-    HTTP_OK = 200,
-    HTTP_PARTIAL_CONTENT = 206,
-
     HTTP_CLASS__REDIRECT = 300,
-    HTTP_MOVED_PERMANENTLY = 301,
-    HTTP_TEMPORARY_REDIRECT = 307,
-    HTTP_NOT_MODIFIED = 304,
-
     HTTP_CLASS__CLIENT_ERROR = 400,
-    HTTP_BAD_REQUEST = 400,
-    HTTP_NOT_AUTHORIZED = 401,
-    HTTP_FORBIDDEN = 403,
-    HTTP_NOT_FOUND = 404,
-    HTTP_NOT_ALLOWED = 405,
-    HTTP_TIMEOUT = 408,
-    HTTP_TOO_LARGE = 413,
-    HTTP_RANGE_UNSATISFIABLE = 416,
-    HTTP_I_AM_A_TEAPOT = 418,
-    HTTP_CLIENT_TOO_HIGH = 420,
-
     HTTP_CLASS__SERVER_ERROR = 500,
-    HTTP_INTERNAL_ERROR = 500,
-    HTTP_NOT_IMPLEMENTED = 501,
-    HTTP_UNAVAILABLE = 503,
-    HTTP_SERVER_TOO_HIGH = 520,
+
+    FOR_EACH_HTTP_STATUS(GENERATE_ENUM_ITEM)
 };
+#undef GENERATE_ENUM_ITEM
 
 enum lwan_handler_flags {
     HANDLER_HAS_POST_DATA = 1 << 0,
