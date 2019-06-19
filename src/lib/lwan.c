f3caad206f56bfd1c33b4c5a45acd7d9dd493fc7
Leandro Pereira <leandro@hardinfo.org>
2019-06-19 07:36:48 -0700

    Slightly shorter code to loop over set bits in core_bitmap struct
    Generated assembly should be the same; this is just more compact C.

--- src/lib/lwan.c
+++ src/lib/lwan.c
@@ -702,14 +702,9 @@ static int accept_connection_coro(struct coro *coro, void *data)
             break;
 
         for (size_t i = 0; i < N_ELEMENTS(cores.bitmap); i++) {
-            uint64_t c = cores.bitmap[i];
-
-            while (c) {
+            for (uint64_t c = cores.bitmap[i]; c; c ^= c & -c) {
                 size_t core = (size_t)__builtin_ctzl(c);
-
                 lwan_thread_nudge(&l->thread.threads[i * 64 + core]);
-
-                c ^= c & -c;
             }
         }
         memset(&cores, 0, sizeof(cores));
