1226f8826769acf7f2a0cf5f2a344885bead687f
Leandro Pereira <leandro@hardinfo.org>
2019-09-08 19:30:20 -0700

    Use &array->base instead of downcasting
    It's clearer which type should be passed to lwan_array_*() functions
    this way.

--- src/lib/lwan-array.h
+++ src/lib/lwan-array.h
@@ -56,17 +56,17 @@ struct lwan_array *coro_lwan_array_new(struct coro *coro);
     __attribute__((unused)) static inline int array_type_##_init(              \
         struct array_type_ *array)                                             \
     {                                                                          \
-        return lwan_array_init((struct lwan_array *)array);                    \
+        return lwan_array_init(&array->base);                                  \
     }                                                                          \
     __attribute__((unused)) static inline int array_type_##_reset(             \
         struct array_type_ *array)                                             \
     {                                                                          \
-        return lwan_array_reset((struct lwan_array *)array);                   \
+        return lwan_array_reset(&array->base);                                 \
     }                                                                          \
     __attribute__((unused)) static inline element_type_ *array_type_##_append( \
         struct array_type_ *array)                                             \
     {                                                                          \
-        return (element_type_ *)lwan_array_append((struct lwan_array *)array,  \
+        return (element_type_ *)lwan_array_append(&array->base,                \
                                                   sizeof(element_type_));      \
     }                                                                          \
     __attribute__((unused)) static inline element_type_                        \
@@ -82,8 +82,7 @@ struct lwan_array *coro_lwan_array_new(struct coro *coro);
     __attribute__((unused)) static inline void array_type_##_sort(             \
         struct array_type_ *array, int (*cmp)(const void *a, const void *b))   \
     {                                                                          \
-        lwan_array_sort((struct lwan_array *)array, sizeof(element_type_),     \
-                        cmp);                                                  \
+        lwan_array_sort(&array->base, sizeof(element_type_), cmp);             \
     }                                                                          \
     __attribute__((unused)) static inline struct array_type_                   \
         *coro_##array_type_##_new(struct coro *coro)                           \
