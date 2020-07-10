a48fc48aa36fb9bd9519a902b592d1babdf16764
Leandro Pereira <leandro@hardinfo.org>
2020-07-09 20:36:22 -0700

    Get rid of struct lwan_pubsub_sub_queue
    It contained only one element, and can be easily embedded in struct
    lwan_pubsub_subscriber itself.

--- src/lib/lwan-pubsub.c
+++ src/lib/lwan-pubsub.c
@@ -31,76 +31,72 @@ struct lwan_pubsub_topic {
 
 struct lwan_pubsub_msg {
     struct lwan_value value;
-    int refcount;
+    unsigned int refcount;
 };
 
-DEFINE_RING_BUFFER_TYPE(lwan_pubsub_msg_ref, struct lwan_pubsub_msg *, 16)
+DEFINE_RING_BUFFER_TYPE(lwan_pubsub_msg_ref_ring, struct lwan_pubsub_msg *, 16)
 
-struct lwan_pubsub_sub_queue_rb {
-    struct list_node rb;
-    struct lwan_pubsub_msg_ref ref;
-};
-
-struct lwan_pubsub_sub_queue {
-    struct list_head rbs;
+struct lwan_pubsub_msg_ref {
+    struct list_node ref;
+    struct lwan_pubsub_msg_ref_ring ring;
 };
 
 struct lwan_pubsub_subscriber {
     struct list_node subscriber;
 
     pthread_mutex_t lock;
-    struct lwan_pubsub_sub_queue queue;
+    struct list_head msg_refs;
 };
 
-static void lwan_pubsub_queue_init(struct lwan_pubsub_sub_queue *queue)
+static void lwan_pubsub_queue_init(struct lwan_pubsub_subscriber *sub)
 {
-    list_head_init(&queue->rbs);
+    list_head_init(&sub->msg_refs);
 }
 
-static bool lwan_pubsub_queue_put(struct lwan_pubsub_sub_queue *queue,
+static bool lwan_pubsub_queue_put(struct lwan_pubsub_subscriber *sub,
                                   const struct lwan_pubsub_msg *msg)
 {
-    struct lwan_pubsub_sub_queue_rb *rb;
+    struct lwan_pubsub_msg_ref *ref;
 
-    if (!list_empty(&queue->rbs)) {
+    if (!list_empty(&sub->msg_refs)) {
         /* Try putting the message in the last ringbuffer in this queue: if it's
          * full, will need to allocate a new ring buffer, even if others might
          * have space in them:  the FIFO order must be preserved, and short of
          * compacting the queue at this point -- which will eventually happen
          * as it is consumed -- this is the only option. */
-        rb = list_tail(&queue->rbs, struct lwan_pubsub_sub_queue_rb, rb);
-        if (lwan_pubsub_msg_ref_try_put(&rb->ref, &msg))
+        ref = list_tail(&sub->msg_refs, struct lwan_pubsub_msg_ref, ref);
+        if (lwan_pubsub_msg_ref_ring_try_put(&ref->ring, &msg))
             return true;
     }
 
-    rb = malloc(sizeof(*rb));
-    if (!rb)
+    ref = malloc(sizeof(*ref));
+    if (!ref)
         return false;
 
-    lwan_pubsub_msg_ref_init(&rb->ref);
-    lwan_pubsub_msg_ref_put(&rb->ref, &msg);
-    list_add_tail(&queue->rbs, &rb->rb);
+    lwan_pubsub_msg_ref_ring_init(&ref->ring);
+    lwan_pubsub_msg_ref_ring_put(&ref->ring, &msg);
+    list_add_tail(&sub->msg_refs, &ref->ref);
 
     return true;
 }
 
 static struct lwan_pubsub_msg *
-lwan_pubsub_queue_get(struct lwan_pubsub_sub_queue *queue)
+lwan_pubsub_queue_get(struct lwan_pubsub_subscriber *sub)
 {
-    struct lwan_pubsub_sub_queue_rb *rb, *next;
+    struct lwan_pubsub_msg_ref *ref, *next;
 
-    list_for_each_safe (&queue->rbs, rb, next, rb) {
+    list_for_each_safe (&sub->msg_refs, ref, next, ref) {
         struct lwan_pubsub_msg *msg;
 
-        if (lwan_pubsub_msg_ref_empty(&rb->ref)) {
-            list_del(&rb->rb);
-            free(rb);
+        if (lwan_pubsub_msg_ref_ring_empty(&ref->ring)) {
+            list_del(&ref->ref);
+            free(ref);
             continue;
         }
 
-        msg = lwan_pubsub_msg_ref_get(&rb->ref);
+        msg = lwan_pubsub_msg_ref_ring_get(&ref->ring);
 
-        if (rb->rb.next != rb->rb.prev) {
+        if (ref->ref.next != ref->ref.prev) {
             /* If this segment isn't the last one, try pulling in just one
              * element from the next segment, as there's space in the
              * current segment now.
@@ -116,14 +112,14 @@ lwan_pubsub_queue_get(struct lwan_pubsub_sub_queue *queue)
              * completely (and ultimately reducing it to an empty list
              * without any ring buffers).
              */
-            struct lwan_pubsub_sub_queue_rb *next_rb;
+            struct lwan_pubsub_msg_ref *next_ring;
 
-            next_rb = container_of(rb->rb.next, struct lwan_pubsub_sub_queue_rb, rb);
-            if (!lwan_pubsub_msg_ref_empty(&next_rb->ref)) {
+            next_ring = container_of(ref->ref.next, struct lwan_pubsub_msg_ref, ref);
+            if (!lwan_pubsub_msg_ref_ring_empty(&next_ring->ring)) {
                 const struct lwan_pubsub_msg *next_msg;
 
-                next_msg = lwan_pubsub_msg_ref_get(&next_rb->ref);
-                lwan_pubsub_msg_ref_put(&rb->ref, &next_msg);
+                next_msg = lwan_pubsub_msg_ref_ring_get(&next_ring->ring);
+                lwan_pubsub_msg_ref_ring_put(&ref->ring, &next_msg);
             }
         }
 
@@ -208,7 +204,7 @@ bool lwan_pubsub_publish(struct lwan_pubsub_topic *topic,
         ATOMIC_INC(msg->refcount);
 
         pthread_mutex_lock(&sub->lock);
-        if (!lwan_pubsub_queue_put(&sub->queue, msg)) {
+        if (!lwan_pubsub_queue_put(sub, msg)) {
             lwan_status_warning("Couldn't enqueue message, dropping");
             ATOMIC_DEC(msg->refcount);
         }
@@ -230,7 +226,7 @@ lwan_pubsub_subscribe(struct lwan_pubsub_topic *topic)
         return NULL;
 
     pthread_mutex_init(&sub->lock, NULL);
-    lwan_pubsub_queue_init(&sub->queue);
+    lwan_pubsub_queue_init(sub);
 
     pthread_mutex_lock(&topic->lock);
     list_add(&topic->subscribers, &sub->subscriber);
@@ -244,7 +240,7 @@ struct lwan_pubsub_msg *lwan_pubsub_consume(struct lwan_pubsub_subscriber *sub)
     struct lwan_pubsub_msg *msg;
 
     pthread_mutex_lock(&sub->lock);
-    msg = lwan_pubsub_queue_get(&sub->queue);
+    msg = lwan_pubsub_queue_get(sub);
     pthread_mutex_unlock(&sub->lock);
 
     return msg;
@@ -263,7 +259,7 @@ static void lwan_pubsub_unsubscribe_internal(struct lwan_pubsub_topic *topic,
         pthread_mutex_unlock(&topic->lock);
 
     pthread_mutex_lock(&sub->lock);
-    while ((iter = lwan_pubsub_queue_get(&sub->queue)))
+    while ((iter = lwan_pubsub_queue_get(sub)))
         lwan_pubsub_msg_done(iter);
     pthread_mutex_unlock(&sub->lock);
 
