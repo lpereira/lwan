a8605e2c3bfa18ce57c36665c574b8208c2b163c
pontscho <pontscho@gmail.com>
2021-05-20 14:27:50 +0200

    adding syslog() support

--- src/lib/lwan-status.c
+++ src/lib/lwan-status.c
@@ -1,6 +1,6 @@
 /*
  * lwan - simple web server
- * Copyright (c) 2013 Leandro A. F. Pereira <leandro@hardinfo.org>
+ * Copyright (c) 2021 Leandro A. F. Pereira <leandro@hardinfo.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -17,7 +17,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  */
-
 #define _GNU_SOURCE
 #include <errno.h>
 #include <libgen.h>
@@ -30,6 +29,7 @@
 #include <unistd.h>
 
 #include "lwan-private.h"
+#include "lwan-status.h"
 
 enum lwan_status_type {
     STATUS_INFO = 0,
@@ -42,10 +42,10 @@ enum lwan_status_type {
     STATUS_CRITICAL = 8,
 };
 
+static bool can_use_colors(void);
+
 static volatile bool quiet = false;
 static bool use_colors;
-
-static bool can_use_colors(void);
 
 void lwan_status_init(struct lwan *l)
 {
@@ -134,6 +134,87 @@ static long gettid_cached(void)
 
 #define FORMAT_WITH_COLOR(fmt, color) "\033[" color "m" fmt "\033[0m"
 
+#ifdef HAVE_SYSLOG
+
+#include <syslog.h>
+
+static int status_to_syslog_prio[] = {
+    [STATUS_CRITICAL + STATUS_PERROR] = LOG_CRIT,
+    [STATUS_CRITICAL] = LOG_CRIT,
+    [STATUS_ERROR] = LOG_ERR,
+    [STATUS_WARNING] = LOG_WARNING,
+    [STATUS_INFO] = LOG_INFO,
+    [STATUS_DEBUG] = LOG_DEBUG,
+};
+
+#define APPEND(func, fmt, ...)                                                 \
+    len = func(tmp + offs, (unsigned long)(log_len - offs), fmt, __VA_ARGS__); \
+    if (len >= log_len - offs - 1) {                                           \
+        log_len *= 2;                                                          \
+        continue;                                                              \
+    } else if (len < 0) {                                                      \
+        return;                                                                \
+    }                                                                          \
+    offs += len;
+
+void lwan_syslog_status_out(
+#ifndef NDEBUG
+    const char *file,
+    const int line,
+    const char *func,
+    const long tid,
+#endif
+    enum lwan_status_type type,
+    int saved_errno,
+    const char *fmt,
+    va_list values)
+{
+    static volatile int log_len = 256;
+    char *tmp = NULL;
+    char *errmsg = NULL;
+
+#ifndef NDEBUG
+    char *base_name = basename(strdupa(file));
+#endif
+
+    if (UNLIKELY(type & STATUS_PERROR)) {
+        char errbuf[64];
+        errmsg = strerror_thunk_r(saved_errno, errbuf, sizeof(errbuf) - 1);
+    }
+
+    do {
+        va_list copied_values;
+        va_copy(copied_values, values);
+
+        tmp = alloca((size_t)log_len);
+
+        int len = 0;
+        int offs = 0;
+
+#ifndef NDEBUG
+        APPEND(snprintf, "%ld %s:%d %s() ", tid, base_name, line, func)
+#endif
+
+        APPEND(vsnprintf, fmt, copied_values)
+
+        if (errmsg) {
+            APPEND(snprintf, ": %s (error number %d)", errmsg, saved_errno)
+        }
+    } while (0);
+
+    syslog(status_to_syslog_prio[type], "%s", tmp);
+}
+
+#undef APPEND
+
+__attribute__((constructor)) static void register_lwan_to_syslog(void)
+{
+    openlog("lwan", LOG_NDELAY | LOG_PID | LOG_CONS, LOG_USER);
+}
+#else
+#define lwan_syslog_status_out(...)
+#endif
+
 static void status_out(
 #ifndef NDEBUG
     const char *file,
@@ -147,6 +228,13 @@ static void status_out(
     struct lwan_value start = start_color(type);
     struct lwan_value end = end_color();
     int saved_errno = errno;
+
+#ifndef NDEBUG
+    lwan_syslog_status_out(file, line, func, gettid_cached(), type, saved_errno,
+                           fmt, values);
+#else
+    lwan_syslog_status_out(type, saved_errno, fmt, values);
+#endif
 
     flockfile(stdout);
 
