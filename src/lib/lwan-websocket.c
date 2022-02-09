f124c72ecedc84856ba8a15138e8c49800285489
Zoltan Ponekker <zoltan.ponekker@doclerholding.com>
2022-02-09 17:33:26 +0100

    WebSocket: tiny performance optimalisation in write_websocket_frame()

--- src/lib/lwan-websocket.c
+++ src/lib/lwan-websocket.c
@@ -56,29 +56,27 @@ enum ws_opcode {
 };
 
 static void write_websocket_frame(struct lwan_request *request,
-                                  unsigned char header_byte,
+                                  unsigned char op,
                                   char *msg,
                                   size_t len)
 {
-    uint8_t frame[9];
+    uint8_t frame[10] = { op };
     size_t frame_len;
 
     if (len <= 125) {
-        frame[0] = (uint8_t)len;
-        frame_len = 1;
+        frame[1] = (uint8_t)len;
+        frame_len = 2;
     } else if (len <= 65535) {
-        frame[0] = 0x7e;
-        memcpy(frame + 1, &(uint16_t){htons((uint16_t)len)}, sizeof(uint16_t));
-        frame_len = 3;
+        frame[1] = 0x7e;
+        *((uint16_t *)(frame + 2)) = htons((uint16_t)len);
+        frame_len = 4;
     } else {
-        frame[0] = 0x7f;
-        memcpy(frame + 1, &(uint64_t){htobe64((uint64_t)len)},
-               sizeof(uint64_t));
-        frame_len = 9;
+        frame[1] = 0x7f;
+        *((uint64_t *)(frame + 2)) = htobe64((uint64_t)len);
+        frame_len = 10;
     }
 
     struct iovec vec[] = {
-        {.iov_base = &header_byte, .iov_len = 1},
         {.iov_base = frame, .iov_len = frame_len},
         {.iov_base = msg, .iov_len = len},
     };
