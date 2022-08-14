23ea3286ab1ecbc3d3ea9ef130d0307135b7a349
L. Pereira <l@tia.mat.br>
2022-08-14 16:20:44 -0700

    Reduce allocations in pastebin sample by using int64 keys

--- src/samples/pastebin/main.c
+++ src/samples/pastebin/main.c
@@ -51,7 +51,7 @@ static struct hash *pending_pastes(void)
     static __thread struct hash *pending_pastes;
 
     if (!pending_pastes) {
-        pending_pastes = hash_str_new(NULL, NULL);
+        pending_pastes = hash_int64_new(NULL, NULL);
         if (!pending_pastes) {
             lwan_status_critical(
                 "Could not allocate pending pastes hash table");
@@ -63,7 +63,8 @@ static struct hash *pending_pastes(void)
 
 static struct cache_entry *create_paste(const char *key, void *context)
 {
-    const struct lwan_value *body = hash_find(pending_pastes(), key);
+    const struct lwan_value *body =
+        hash_find(pending_pastes(), (const void *)key);
     size_t alloc_size;
 
     if (!body)
@@ -90,38 +91,31 @@ static void destroy_paste(struct cache_entry *entry, void *context)
 
 static void remove_from_pending(void *data)
 {
-    const char *key = data;
-
-    hash_del(pending_pastes(), key);
+    hash_del(pending_pastes(), data);
 }
 
 static enum lwan_http_status post_paste(struct lwan_request *request,
                                         struct lwan_response *response)
 {
     const struct lwan_value *body = lwan_request_get_request_body(request);
-    char key_buf[3 * sizeof(uint64_t)];
-    size_t key_len;
 
     if (!body)
         return HTTP_BAD_REQUEST;
 
     for (int try = 0; try < 10; try++) {
-        /* Copy so that the key is valid even when this handler function
-         * ends. */
-        char *key = coro_strdup(
-            request->conn->coro,
-            uint_to_string(lwan_random_uint64(), key_buf, &key_len));
+        uint64_t key = lwan_random_uint64();
 
-        if (!hash_add_unique(pending_pastes(), key, body)) {
-            coro_defer(request->conn->coro, remove_from_pending, key);
+        if (!hash_add_unique(pending_pastes(), &key, body)) {
+            coro_defer(request->conn->coro, remove_from_pending,
+                       (void *)(uintptr_t)key);
 
-            struct cache_entry *paste =
-                cache_coro_get_and_ref_entry(pastes, request->conn->coro, key);
+            struct cache_entry *paste = cache_coro_get_and_ref_entry(
+                pastes, request->conn->coro, (void *)(uintptr_t)key);
 
             if (paste) {
                 response->mime_type = "text/plain";
-                lwan_strbuf_printf(response->buffer, "http://%s:%d/p/%s",
-                                   SERVER_NAME, SERVER_PORT, key);
+                lwan_strbuf_printf(response->buffer, "https://%s/p/%zu\n\n",
+                                   SERVER_NAME, key);
                 return HTTP_OK;
             }
         }
@@ -175,8 +169,9 @@ LWAN_HANDLER(view_paste)
         mime_type = "text/plain";
     }
 
+    const int64_t key = parse_long_long(request->url.value, 0);
     struct paste *paste = (struct paste *)cache_coro_get_and_ref_entry(
-        pastes, request->conn->coro, request->url.value);
+        pastes, request->conn->coro, (void *)(uintptr_t)key);
 
     if (!paste)
         return HTTP_NOT_FOUND;
@@ -199,8 +194,9 @@ int main(void)
 
     lwan_init(&l);
 
-    pastes = cache_create(create_paste, destroy_paste, NULL,
-                          CACHE_FOR_HOURS * 60 * 60);
+    pastes = cache_create_full(create_paste, destroy_paste, NULL,
+                               CACHE_FOR_HOURS * 60 * 60,
+                               hash_int64_new);
     if (!pastes)
         lwan_status_critical("Could not create paste cache");
 
