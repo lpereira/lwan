2b7794cccc82e5f8d012da0c1c83e707d131b801
L. Pereira <l@tia.mat.br>
2026-08-24 13:15:33 -0300

    Remove the COLORTERM environment variable support
    This wasn't that great to begin with. Let's just go with TERM=dumb
    or NO_COLOR as that seems better.

--- src/lib/lwan-log.c
+++ src/lib/lwan-log.c
@@ -67,7 +67,6 @@ static bool can_use_colors(void)
 {
     const char *term;
     const char *no_color;
-    const char *colorterm;
 
     if (!isatty(fileno(stdout)))
         return false;
@@ -79,14 +78,6 @@ static bool can_use_colors(void)
     no_color = secure_getenv("NO_COLOR");
     if (no_color && no_color[0])
         return false;
-
-    colorterm = secure_getenv("COLORTERM");
-    if (colorterm) {
-        if (streq(colorterm, "truecolor"))
-            return true;
-        if (streq(colorterm, "24bit"))
-            return true;
-    }
 
     term = secure_getenv("TERM");
     if (term && streq(term, "dumb"))
